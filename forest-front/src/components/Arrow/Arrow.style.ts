54a6f7cb46ee3034dcb847b085f9c6cc8be466d4
HeeJeYang <ghjadmb21@gmail.com>
2023-04-25 02:19:30 +0900

    [FE-2002] feat: 화살표 세로 가운데 정렬
    Former-commit-id: 2eaddaf59c647cf1a502269dac922a13a11244fe

--- forest-front/src/components/Arrow/Arrow.style.ts
+++ forest-front/src/components/Arrow/Arrow.style.ts
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 
 const ArrowIcon = styled.span`
+  position: relative;
   cursor: pointer;
   font-size: 2rem;
+  height: 2rem;
   color: ${({ theme }) => theme.colors.Lime[900]};
 `;
 
