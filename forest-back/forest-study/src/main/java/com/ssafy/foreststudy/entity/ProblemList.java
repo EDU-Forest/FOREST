00a2b8754aa06667cf076068635627a79fab42c4
Leeh9 <40704078+Leeh9@users.noreply.github.com>
2023-04-25 02:33:05 +0900

    [BE-3002] feat: problem entity 작성

---
+++ forest-back/forest-study/src/main/java/com/ssafy/foreststudy/entity/ProblemList.java
@@ -0,0 +1,37 @@
+package com.ssafy.foreststudy.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "problem_lists")
+public class ProblemList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "bigint", nullable = false)
+    private Long id;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "workbook_id", nullable = false)
+    private Workbook workbook;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "problem_id", nullable = false)
+    private Problem problem;
+
+    @Column(name = "problem_num", columnDefinition = "int", nullable = false)
+    private int problemNum;
+
+    @Column(name = "is_deleted", columnDefinition = "tinyint(1) default 0", nullable = false)
+    private Boolean isDeleted = false;
+}
