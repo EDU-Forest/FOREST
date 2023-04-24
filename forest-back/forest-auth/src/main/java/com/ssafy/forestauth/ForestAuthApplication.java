0c05c7e46e062ceb848ea82acad3e6a7784c48a1
choi1087 <adcdm87@gmail.com>
2023-04-24 19:58:13 +0900

    [BE-3001] feat: auth project 생성

---
+++ forest-back/forest-auth/src/main/java/com/ssafy/forestauth/ForestAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.forestauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForestAuthApplication.class, args);
+    }
+
+}
---
+++ forest-back/forest-auth/src/test/java/com/ssafy/forestauth/ForestAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.forestauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForestAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
