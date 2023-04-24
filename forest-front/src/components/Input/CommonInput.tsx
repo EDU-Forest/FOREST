02afa291f70ebdb0ead61b36b187c11232581d0c
HeeJeYang <ghjadmb21@gmail.com>
2023-04-25 02:21:04 +0900

    [FE-2002] feat: id, name 추가
    Former-commit-id: c20025ec2f43c18bc1e1c4ae728e04a39a2df401

--- forest-front/src/components/Input/CommonInput.tsx
+++ forest-front/src/components/Input/CommonInput.tsx
@@ -1,12 +1,22 @@
 import { StyledCommonInput } from "./Input.style";
 
 interface Iprops {
+  id: string;
+  name: string;
   placeholder: string;
   inputText: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
   // onKeyDown: (event: React.KeyboardEvent<HTMLDivElement>) => void;
 }
 
-export default function CommonInput({ placeholder, inputText, onChange }: Iprops) {
-  return <StyledCommonInput placeholder={placeholder} value={inputText} onChange={onChange} />;
+export default function CommonInput({ id, name, placeholder, inputText, onChange }: Iprops) {
+  return (
+    <StyledCommonInput
+      id={id}
+      name={name}
+      placeholder={placeholder}
+      value={inputText}
+      onChange={onChange}
+    />
+  );
 }
