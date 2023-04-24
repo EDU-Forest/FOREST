a52823840a81f027b2cefc3d90949a6b651db1e9
Leeh9 <40704078+Leeh9@users.noreply.github.com>
2023-04-25 02:33:19 +0900

    [BE-3002] feat: workbook entity 작성

---
+++ forest-back/forest-study/src/main/java/com/ssafy/foreststudy/entity/Workbook.java
@@ -0,0 +1,62 @@
+package com.ssafy.foreststudy.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "workbooks")
+@EntityListeners(AuditingEntityListener.class)
+public class Workbook {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "bigint", nullable = false)
+    private Long id;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "workbook_imgs_id", nullable = false)
+    private WorkbookImg workbookImg;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "creator_id", nullable = false)
+    private User creator;
+
+    @Column(name = "title", columnDefinition = "varchar(30)", nullable = false)
+    private String title;
+
+    @Column(name = "desc", columnDefinition = "varchar(255)")
+    private String desc;
+
+    @Column(name = "volume", columnDefinition = "int default 0", nullable = false)
+    private int volume;
+
+    @Column(name = "is_executed", columnDefinition = "tinyint(1) default 0", nullable = false)
+    private Boolean isExecuted = false;
+
+    @Column(name = "is_public", columnDefinition = "tinyint(1) default 0", nullable = false)
+    private Boolean isPublic = false;
+
+    @CreatedDate
+    @Column(name = "created_date", updatable = false, nullable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "updated_date", updatable = false, nullable = false)
+    private LocalDateTime updatedDate;
+
+    @Column(name = "is_deleted", columnDefinition = "tinyint(1) default 0", nullable = false)
+    private Boolean isDeleted = false;
+}
