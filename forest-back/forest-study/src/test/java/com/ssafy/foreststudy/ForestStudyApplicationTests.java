3b20a4d79a118e22bdaa7e959c751b5a7ec3dea8
gusrnqq@naver.com <gusrnqq@naver.com>
2023-04-24 17:16:14 +0900

    BE-3001 feat: gradle 설정 추가

---
+++ forest-back/forest-study/src/test/java/com/ssafy/foreststudy/ForestStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.foreststudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForestStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
