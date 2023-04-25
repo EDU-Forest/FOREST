def96be55317724b6b68174baf83b5f551badc47
HeeJeYang <ghjadmb21@gmail.com>
2023-04-26 07:03:49 +0900

    [FE-2002] feat: CommonInput 스타일화
    Former-commit-id: 660c3cf426afac3637d9f712e435975e21f6534b

--- forest-front/src/components/Input/CommonInput.tsx
+++ forest-front/src/components/Input/CommonInput.tsx
@@ -1,22 +1,31 @@
-import { StyledCommonInput } from "./Input.style";
+// import { StyledCommonInput } from "./Input.style";
 
-interface Iprops {
-  id: string;
-  name: string;
-  placeholder: string;
-  inputText: string;
-  onChange: React.ChangeEventHandler<HTMLInputElement>;
-  // onKeyDown: (event: React.KeyboardEvent<HTMLDivElement>) => void;
-}
+// interface Iprops {
+//   id: string;
+//   name: string;
+//   type?: string;
+//   placeholder: string;
+//   inputText: string;
+//   onChange: React.ChangeEventHandler<HTMLInputElement>;
+//   // onKeyDown: (e: React.KeyboardEvent<HTMLDivElement>) => void;
+// }
 
-export default function CommonInput({ id, name, placeholder, inputText, onChange }: Iprops) {
-  return (
-    <StyledCommonInput
-      id={id}
-      name={name}
-      placeholder={placeholder}
-      value={inputText}
-      onChange={onChange}
-    />
-  );
-}
+// export default function CommonInput({
+//   id,
+//   name,
+//   type = "text",
+//   placeholder,
+//   inputText,
+//   onChange,
+// }: Iprops) {
+//   return (
+//     <StyledCommonInput
+//       id={id}
+//       name={name}
+//       type={type}
+//       placeholder={placeholder}
+//       value={inputText}
+//       onChange={onChange}
+//     />
+//   );
+// }
