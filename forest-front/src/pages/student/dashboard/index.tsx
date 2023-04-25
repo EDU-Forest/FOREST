0b13bf87a174f506bfd8a36fd5583a0e9b620aa7
aeebbr <aeebbr@gmail.com>
2023-04-26 03:41:43 +0900

    [FE-1003] feat: 학생 대시보드 구현
    Former-commit-id: 97cc39fcc58208ade0149e42afe3f1d4e943538d

--- forest-front/src/pages/student/dashboard/index.tsx
+++ forest-front/src/pages/student/dashboard/index.tsx
@@ -1,11 +1,24 @@
 import StudentNav from "@/components/Nav/StudentNav";
-import { Container, FullScreen } from "@/styles/container";
+import { FullScreen } from "@/styles/container";
+import {
+  StyledDashboardContainer,
+  StyledDashboardSectionFlexBox,
+} from "@/features/dashboard/Dashboard.style";
+import DashboardBanner from "@/features/dashboard/DashboardBanner";
+import Schedule from "@/features/dashboard/Schedule";
+import DashboardClass from "@/features/dashboard/student/DashboardClass";
 
 export default function StudentDashBoard() {
   return (
     <FullScreen>
       <StudentNav nowLocation={"dashboard"} />
-      <Container></Container>
+      <StyledDashboardContainer padding={2}>
+        <DashboardBanner role="학생" />
+        <StyledDashboardSectionFlexBox>
+          <Schedule />
+          <DashboardClass />
+        </StyledDashboardSectionFlexBox>
+      </StyledDashboardContainer>
     </FullScreen>
   );
 }
