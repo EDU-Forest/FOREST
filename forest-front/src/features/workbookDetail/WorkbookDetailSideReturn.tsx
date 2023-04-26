b4ca7c3acb5c251c7c31a617c4a93317c22b5df1
임경민 <aeebbr@gmail.com>
2023-04-26 17:57:10 +0900

    [FE-1004] feat: 문제집 페이지 좌측 화살표  컴포넌트 구현
    Former-commit-id: b435ae8dde6b0dbc1165991f252941ba951f590d

---
+++ forest-front/src/features/workbookDetail/WorkbookDetailSideReturn.tsx
@@ -0,0 +1,11 @@
+import { AiOutlineArrowLeft } from "react-icons/ai";
+
+function WorkbookDetailSideReturn() {
+  return (
+    <div>
+      <AiOutlineArrowLeft />
+    </div>
+  );
+}
+
+export default WorkbookDetailSideReturn;
