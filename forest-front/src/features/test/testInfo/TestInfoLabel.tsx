9cf78466c3ae9d84a98a8eebf25a591822658808
HeeJeYang <ghjadmb21@gmail.com>
2023-04-27 06:59:07 +0900

    [FE-2004] feat: 문제풀이 대문 페이지 라벨 제작
    Former-commit-id: da731a8b5c20b7100d3d66bf0a7f118e9d964a93

---
+++ forest-front/src/features/test/testInfo/TestInfoLabel.tsx
@@ -0,0 +1,13 @@
+// import { StyledTestInfoLabel, StyledTestInfoText } from "./TestInfo.style";
+
+// interface Iprops {
+//   text: string;
+// }
+
+// export default function TestInfoLabel({text}:Iprops) {
+//   return (
+//     <StyledTestInfoLabel>
+//       <StyledTestInfoText>{text}</StyledTestInfoText>
+//     </StyledTestInfoLabel>
+//   )
+// }
