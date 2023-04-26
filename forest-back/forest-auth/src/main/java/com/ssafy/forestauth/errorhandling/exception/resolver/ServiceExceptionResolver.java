001122bb529b8a436f405a19ed6fde6701383aee
choi1087 <adcdm87@gmail.com>
2023-04-26 17:54:49 +0900

    [BE-1001] feat: pw 불일치 에러 추가

--- forest-back/forest-auth/src/main/java/com/ssafy/forestauth/errorhandling/exception/resolver/ServiceExceptionResolver.java
+++ forest-back/forest-auth/src/main/java/com/ssafy/forestauth/errorhandling/exception/resolver/ServiceExceptionResolver.java
@@ -3,6 +3,7 @@ package com.ssafy.forestauth.errorhandling.exception.resolver;
 import com.ssafy.forestauth.dto.common.response.ResponseErrorDto;
 import com.ssafy.forestauth.enumeration.response.ErrorCode;
 import com.ssafy.forestauth.errorhandling.exception.service.EntityIsNullException;
+import com.ssafy.forestauth.errorhandling.exception.service.InvalidPasswordException;
 import com.ssafy.forestauth.mattermost.NotificationManager;
 import com.ssafy.forestauth.util.ResponseUtil;
 import lombok.RequiredArgsConstructor;
@@ -33,6 +34,14 @@ public class ServiceExceptionResolver {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = EntityIsNullException.class)
     public ResponseErrorDto<?> handle(EntityIsNullException e, HttpServletRequest request) {
+        ErrorCode errorCode = e.getErrorCode();
+        notificationManager.sendNotification(e, request.getRequestURI(), getParams(request));
+        return responseUtil.buildErrorResponse(errorCode, request.getRequestURI());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = InvalidPasswordException.class)
+    public ResponseErrorDto<?> handle(InvalidPasswordException e, HttpServletRequest request) {
         ErrorCode errorCode = e.getErrorCode();
         notificationManager.sendNotification(e, request.getRequestURI(), getParams(request));
         return responseUtil.buildErrorResponse(errorCode, request.getRequestURI());
