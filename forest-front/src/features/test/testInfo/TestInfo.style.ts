9a371e88d4b4fcb5de1b2d63e97aa607bcc1fa8e
HeeJeYang <ghjadmb21@gmail.com>
2023-04-27 07:00:11 +0900

    [FE-2004] feat: 문제풀이 대문 페이지 css 작성
    Former-commit-id: 136b0e042650a6b99db55653c23067b9dd6984af

--- forest-front/src/features/test/testInfo/TestInfo.style.ts
+++ forest-front/src/features/test/testInfo/TestInfo.style.ts
@@ -1,18 +1,19 @@
 import styled from "styled-components";
 
-const TestInfoWrapper = styled.div`
+const StyledTestInfoBox = styled.div`
   width: 32.5rem;
-  padding: 1rem, 3.5rem;
+  padding: 0.75rem 3.5rem;
+
   background-color: ${({ theme }) => theme.colors.Lime[100]};
-  box-shadow: 0.25rem 0.25rem 0.25rem rgba(157, 157, 157, 0.25);
-  border-radius: 1.5rem;
+  box-shadow: 4px 4px 4px rgba(157, 157, 157, 0.25);
+  border-radius: 24px;
 `;
 
 const StyledTestInfoContainer = styled.div`
-  width: 50rem;
-  padding: 6.5rem 8.75rem;
-  box-shadow: 10px 10px 10px 4px rgba(0, 0, 0, 0.15);
-  border-radius: 1rem;
+  width: 600px;
+  padding: 4rem 6rem;
+  box-shadow: 0.625rem 0.625rem 0.625rem 0.25rem rgba(0, 0, 0, 0.15);
+  border-radius: 16px;
   background-color: #ffffff;
   display: flex;
   flex-direction: column;
@@ -20,14 +21,62 @@ const StyledTestInfoContainer = styled.div`
 `;
 
 const TestStartImg = styled.img`
-  width: 22.5rem;
-  height: 400px;
+  width: 10rem;
+  height: 10rem;
 `;
 
 const StyledTestInfoTitleBox = styled.div`
-  margin: 24px;
-  font-size: 2rem;
+  margin: 32px 16px;
+  font-size: 22px;
   font-weight: 700;
 `;
 
-export { TestInfoWrapper, StyledTestInfoContainer, TestStartImg, StyledTestInfoTitleBox };
+const StyledTestInfoContent = styled.div`
+  padding: 0.75rem 0rem;
+  display: flex;
+  align-items: center;
+`;
+
+const StyledTestInfoLabel = styled.div`
+  width: 120px;
+  height: 32px;
+  border-radius: 16px;
+  background-color: ${({ theme }) => theme.colors.Lime[700]};
+  color: white;
+  font-size: 14px;
+  text-align: center;
+  line-height: 32px;
+  margin-right: 32px;
+`;
+
+const StyledTestInfoText = styled.div`
+  font-size: 16px;
+`;
+
+const StyledTestInfoBtn = styled.button`
+  width: 15rem;
+  height: 2.5rem;
+  background-color: ${({ theme }) => theme.colors.Lime[700]};
+  border: none;
+  border-radius: 1.5rem;
+  line-height: 2.5rem;
+  text-align: center;
+  color: white;
+  margin-top: 3rem;
+  font-weight: 700;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.Lime[900]};
+  }
+`;
+
+export {
+  StyledTestInfoBox,
+  StyledTestInfoContainer,
+  TestStartImg,
+  StyledTestInfoTitleBox,
+  StyledTestInfoLabel,
+  StyledTestInfoContent,
+  StyledTestInfoText,
+  StyledTestInfoBtn,
+};
