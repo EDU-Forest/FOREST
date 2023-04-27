8e047b0ab196e1510930cdcdef35a40c54d0d155
이희진 <oliqoeiilo@naver.com>
2023-04-27 10:38:00 +0900

    [BE-2001] feat: init

---
+++ forest-back/forest-workbook/src/main/java/com/ssafy/forestworkbook/ForestWorkbookApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.forestworkbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestWorkbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForestWorkbookApplication.class, args);
+    }
+
+}
