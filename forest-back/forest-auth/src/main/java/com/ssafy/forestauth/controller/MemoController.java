2e1bba5d6501e83f93dbec6f62e2abbc6193b235
choi1087 <adcdm87@gmail.com>
2023-04-28 17:57:45 +0900

    [BE-1001] feat: memo 관련 로직

--- forest-back/forest-auth/src/main/java/com/ssafy/forestauth/controller/MemoController.java
+++ forest-back/forest-auth/src/main/java/com/ssafy/forestauth/controller/MemoController.java
@@ -1,13 +1,19 @@
 package com.ssafy.forestauth.controller;
 
+import com.ssafy.forestauth.dto.common.response.ResponseSuccessDto;
+import com.ssafy.forestauth.dto.memo.*;
 import com.ssafy.forestauth.service.MemoService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
-@Api("Auth Controller V1")
+import java.util.List;
+
+@Api("Memo Controller V1")
 @RestController
 @Slf4j
 @RequiredArgsConstructor
@@ -15,4 +21,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemoController {
 
     private final MemoService memoService;
+
+    // 메모 조회
+    @GetMapping("")
+    public ResponseEntity<ResponseSuccessDto<List<SelectMemoResponseDto>>> selectMemo(
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        Long userId = Long.parseLong(userDetails.getUsername());
+        return ResponseEntity.ok(memoService.selectMemo(userId));
+    }
+
+    // 메모 생성
+    @PostMapping("")
+    public ResponseEntity<ResponseSuccessDto<SaveMemoResponseDto>> saveMemo(
+            @RequestBody SaveMemoRequestDto saveMemoRequestDto,
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        Long userId = Long.parseLong(userDetails.getUsername());
+        return ResponseEntity.ok(memoService.saveMemo(userId, saveMemoRequestDto));
+    }
+
+    // 메모 삭제
+    @PatchMapping("")
+    public ResponseEntity<ResponseSuccessDto<DeleteMemoResponseDto>> deleteMemo(
+            @RequestBody List<DeleteMemoRequestDto> deleteMemoRequestDtoList,
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        return ResponseEntity.ok(memoService.deleteMemo(deleteMemoRequestDtoList));
+    }
+
 }
