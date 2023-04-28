a1cf871703cd42542acbc788e66ff505095af755
choi1087 <adcdm87@gmail.com>
2023-04-28 17:58:12 +0900

    [BE-1001] feat: cheeringMsg 관련 로직

--- forest-back/forest-auth/src/main/java/com/ssafy/forestauth/service/CheeringMsgService.java
+++ forest-back/forest-auth/src/main/java/com/ssafy/forestauth/service/CheeringMsgService.java
@@ -1,5 +1,9 @@
 package com.ssafy.forestauth.service;
 
+import com.ssafy.forestauth.dto.common.response.ResponseSuccessDto;
+import com.ssafy.forestauth.dto.msg.SelectCheeringMsgResponseDto;
+import com.ssafy.forestauth.entity.CheeringMsg;
+import com.ssafy.forestauth.enumeration.response.SuccessCode;
 import com.ssafy.forestauth.repository.CheeringMsgRepository;
 import com.ssafy.forestauth.util.ResponseUtil;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -16,4 +21,16 @@ public class CheeringMsgService {
 
     private final ResponseUtil responseUtil;
     private final CheeringMsgRepository cheeringMsgRepository;
+
+    public ResponseSuccessDto<SelectCheeringMsgResponseDto> selectMsg() {
+        List<CheeringMsg> msgList = cheeringMsgRepository.findAll();
+        int idx = (int) Math.round(Math.random() * (msgList.size() - 1));
+        CheeringMsg cheeringMsg = msgList.get(idx);
+        SelectCheeringMsgResponseDto cheeringMsgResponseDto = SelectCheeringMsgResponseDto.builder()
+                .content(cheeringMsg.getContent())
+                .build();
+
+        ResponseSuccessDto<SelectCheeringMsgResponseDto> res = responseUtil.successResponse(cheeringMsgResponseDto, SuccessCode.AUTH_READ_CHEERING_MSG);
+        return res;
+    }
 }
