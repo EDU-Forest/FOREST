2e4c9463deb35e2929198b338bcae09f41e0f765
HeeJeYang <ghjadmb21@gmail.com>
2023-04-29 22:16:00 +0900

    [FE-2001] feat: text 전역 스타일 생성
    Former-commit-id: a93070ab454faa7f97d2027eeb4238c92450c420

--- forest-front/src/styles/text.ts
+++ forest-front/src/styles/text.ts
@@ -6,3 +6,9 @@ export const Title = styled.p`
   font-size: 1.375rem;
   font-weight: 700;
 `;
+
+export const Text = (size = 16, isBold = false, isGray = false) => `
+  font-size: ${size / 16}rem;
+  font-weight: ${isBold && 700};
+  color: ${isGray && "#ADB5BD"};
+`;
