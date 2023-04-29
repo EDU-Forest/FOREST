52a954dd1628e7048f03637516c4e19f1b5a1719
이희진 <oliqoeiilo@naver.com>
2023-04-29 09:38:05 +0900

    [BE-2002] refactor: reference project

---
+++ forest-back/forest-reference/src/main/java/com/ssafy/forestreference/enumeration/response/EnumUserProviderStatus.java
@@ -0,0 +1,5 @@
+package com.ssafy.forestreference.enumeration;
+
+public enum EnumUserProviderStatus {
+    LOCAL, KAKAO
+}
---
+++ forest-back/forest-reference/src/main/java/com/ssafy/forestreference/enumeration/response/EnumUserRoleStatus.java
@@ -0,0 +1,5 @@
+package com.ssafy.forestreference.enumeration;
+
+public enum EnumUserRoleStatus {
+    STUDENT, TEACHER
+}
