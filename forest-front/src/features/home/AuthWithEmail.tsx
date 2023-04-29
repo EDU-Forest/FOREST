cd5d7b552a2c13543714138e4dd12dde345ae5dc
HeeJeYang <ghjadmb21@gmail.com>
2023-04-29 22:14:30 +0900

    [FE-2001] feat: 컴포넌트명 수정
    Former-commit-id: ae16f844ab176e13d99cf4053c98b2a180bde9be

---
+++ forest-front/src/features/home/AuthWithEmail.tsx
@@ -0,0 +1,20 @@
+// import { EmailBoxWrapper, GrayText } from "./AuthWithEmail.style";
+// import { useRouter } from "next/router";
+
+// interface Iprops {
+//   openModalHandler: (text: string) => void;
+// }
+
+// export default function AuthWithEmail({ openModalHandler }: Iprops) {
+//   return (
+//     <EmailBoxWrapper>
+//       <GrayText className="link" onClick={() => openModalHandler("signup")}>
+//         이메일 회원가입
+//       </GrayText>
+//       <GrayText>|</GrayText>
+//       <GrayText className="link" onClick={() => openModalHandler("login")}>
+//         이메일 로그인
+//       </GrayText>
+//     </EmailBoxWrapper>
+//   );
+// }
