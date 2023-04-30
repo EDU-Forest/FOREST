06a4fe39347256cd99fdff32d95afbe8889d35f0
HeeJeYang <ghjadmb21@gmail.com>
2023-04-30 20:09:16 +0900

    [FE-2002] feat: 카카오 로그인 연결
    Former-commit-id: aee2003081ac4810dba82e0964578e5148411da6

--- forest-front/src/pages/index.tsx
+++ forest-front/src/pages/index.tsx
@@ -7,23 +7,27 @@ import LoginModal from "@/features/home/LoginModal";
 import SignupModal from "@/features/home/SignupModal";
 // import AuthWithEmail from "@/features/home/AuthWithEmail";
 import { EmailAuthBox } from "@/features/home/Home.style";
+import { useRouter } from "next/router";
 import EmailAuth from "@/features/home/EmailAuth";
 
 export default function Home() {
+  const router = useRouter();
   const [modalState, setModalState] = useState("");
 
   const handleModal = (text: string) => {
     setModalState(text);
   };
 
-  const test = () => {};
+  const goToDash = () => {
+    router.push("/teacher/dashboard");
+  };
 
   return (
     <FullScreen isColumn={true}>
       {modalState === "signup" && <SignupModal onClose={() => handleModal("")} />}
       {modalState === "login" && <LoginModal onClose={() => handleModal("")} />}
       <AuthSection>
-        <ForestLargeLogo src={"/images/Forest_Logo.png"} />
+        <ForestLargeLogo onClick={goToDash} src={"/images/Forest_Logo.png"} />
         <KakaoLogin />
         <EmailAuth handleModal={handleModal} />
       </AuthSection>
