ef63c63373ec42be4c102cde9412596309664244
aeebbr <aeebbr@gmail.com>
2023-05-01 23:51:15 +0900

    [FE-1006] feat: 에디터 미리보기 및 완료 버튼 영역 생성
    Former-commit-id: c0955123fec717dc60f57daf5a3872d2e7652185

---
+++ forest-front/src/features/editor/EditorBtns.tsx
@@ -0,0 +1,13 @@
+import { StyledWorkbookDetailBtnsBox } from "../workbookDetail/WorkbookDetail.style";
+import { EditorBtnsBox } from "./Editor.style";
+
+function EditorBtns() {
+  return (
+    <EditorBtnsBox>
+      <button>미리보기</button>
+      <button>완료</button>
+    </EditorBtnsBox>
+  );
+}
+
+export default EditorBtns;
