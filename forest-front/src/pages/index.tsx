47743c6d018ef16d977b6977ddf6c927ec0e8dce
HeeJeYang <ghjadmb21@gmail.com>
2023-05-01 23:05:44 +0900

    [FE-2006] feat: 회원가입 관련 로직 구현
    Former-commit-id: 977749681171b74b1c46506b0577dbeb02c5ba49

--- forest-front/src/pages/index.tsx
+++ forest-front/src/pages/index.tsx
@@ -1,15 +1,10 @@
-// import EmailBox from "@/features/home/AuthWithEmail";
 import { FullScreen } from "@/styles/container";
 import { ForestLargeLogo, AuthSection } from "../features/home/index.style";
 import KakaoLogin from "@/features/home/KakaoLogin";
 import { useState } from "react";
-import LoginModal from "@/features/home/LoginModal";
-import SignupModal from "@/features/home/Signup";
-// import AuthWithEmail from "@/features/home/AuthWithEmail";
-import { EmailAuthBox } from "@/features/home/Home.style";
+import LoginModal from "@/features/home/Login";
 import { useRouter } from "next/router";
 import EmailAuth from "@/features/home/EmailAuth";
-import Signup from "@/features/home/Signup";
 import UserForm from "@/features/home/UserForm";
 
 export default function Home() {
@@ -17,7 +12,8 @@ export default function Home() {
   const [modalState, setModalState] = useState("");
 
   const handleModal = (text: string) => {
-    setModalState(text);
+    // setModalState(text);
+    router.push(`/${text}`);
   };
 
   const goToDash = () => {
@@ -26,8 +22,8 @@ export default function Home() {
 
   return (
     <FullScreen isColumn={true}>
-      {modalState === "signup" && <UserForm type={"signup"} onClose={() => handleModal("")} />}
-      {modalState === "login" && <LoginModal onClose={() => handleModal("")} />}
+      {/* {modalState === "signup" && <UserForm type={"signup"} onClose={() => handleModal("")} />} */}
+      {/* {modalState === "login" && <LoginModal onClose={() => handleModal("")} />} */}
       <AuthSection>
         <ForestLargeLogo onClick={goToDash} src={"/images/Forest_Logo.png"} />
         <KakaoLogin />
