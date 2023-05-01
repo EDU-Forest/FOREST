df8b3f3e57931d00e736df6d36fed9185bb1a81d
임경민 <aeebbr@gmail.com>
2023-05-01 17:13:46 +0900

    [FE-1006] fix: 공통 객관식 보기 컴포넌트에서 에디터 모드 분리
    Former-commit-id: 557abfa72be87ff34249669afd5795af05242ad2

--- forest-front/src/components/Question/QuestionChoiceList.tsx
+++ forest-front/src/components/Question/QuestionChoiceList.tsx
@@ -4,20 +4,12 @@ import {
   StyledQuestionDetailChoiceListBox,
 } from "@/features/workbookDetail/WorkbookDetail.style";
 import { QuestionItemType } from "@/types/Workbook";
-import { useState } from "react";
 
 interface IProps {
   items: QuestionItemType[];
-  isEdit?: boolean;
 }
 
-function QuestionChoiceList({ items, isEdit }: IProps) {
-  const [content, setContent] = useState("");
-
-  const handleChangeItems = (i: number, e: React.ChangeEvent<HTMLInputElement>) => {
-    items.splice(i, 1, { ...items[i], content: e.target.value });
-  };
-
+function QuestionChoiceList({ items }: IProps) {
   return (
     <StyledQuestionDetailChoiceListBox>
       {items.map((item, i) => {
@@ -25,17 +17,7 @@ function QuestionChoiceList({ items, isEdit }: IProps) {
           <StyledQuestionDetailChoiceBox>
             <StyledQuestionChoiceNumBox>{i + 1}</StyledQuestionChoiceNumBox>
             {/* 이미지 형식의 보기라면 이미지 렌더링 */}
-            {item.isImage ? (
-              <img src={item.content} alt="item" />
-            ) : isEdit ? (
-              <input
-                value={item.content}
-                placeholder="보기를 입력하세요"
-                onChange={(e) => handleChangeItems(i, e)}
-              />
-            ) : (
-              <span>{item.content}</span>
-            )}
+            {item.isImage ? <img src={item.content} alt="item" /> : <span>{item.content}</span>}
           </StyledQuestionDetailChoiceBox>
         );
       })}
