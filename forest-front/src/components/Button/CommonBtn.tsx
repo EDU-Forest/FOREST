70c4a4d78b4ddee2427d18b660b80eca2421edf2
HeeJeYang <ghjadmb21@gmail.com>
2023-05-01 16:23:09 +0900

    [FE-2006] feat: 버튼 컴포넌트 수정
    Former-commit-id: 36730fe1e05e810493564f64692f6e40c7335c50

--- forest-front/src/components/Button/CommonBtn.tsx
+++ forest-front/src/components/Button/CommonBtn.tsx
@@ -4,11 +4,12 @@ interface Iprops {
   children: string;
   colored?: boolean;
   onClick: () => void;
+  disabled: boolean;
 }
 
-export default function CommonBtn({ children, colored, onClick }: Iprops) {
+export default function CommonBtn({ children, colored, onClick, disabled }: Iprops) {
   return (
-    <StyledCommonBtn colored={colored} onClick={onClick}>
+    <StyledCommonBtn colored={colored} onClick={onClick} disabled={disabled}>
       {children}
     </StyledCommonBtn>
   );
