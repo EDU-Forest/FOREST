0d17fb9cd42687381e5fd77d299eca1ba2d6f219
HeeJeYang <ghjadmb21@gmail.com>
2023-05-01 11:07:10 +0900

    [FE-2006] fix: 로그인 쿼리 수정
    Former-commit-id: b3499bbc830aefc4138229fd81c6875476dad8a8

--- forest-front/src/apis/auth/useKakaoLoginQuery.ts
+++ forest-front/src/apis/auth/useKakaoLoginQuery.ts
@@ -11,9 +11,10 @@ const fetcher = () =>
 //   return res;
 // });
 
-const useKakaoLoginQuery = () => {
-  return useQuery(queryKeys.KAKAO_LOGIN, fetcher, {
+const useKakaoLoginQuery = (isClicked: boolean) => {
+  return useQuery([queryKeys.KAKAO_LOGIN, isClicked], fetcher, {
     // react-query는 사용자가 사용하는 윈도우가 다른 곳을 갔다가 다시 화면으로 돌아오면 이 함수를 재실행합니다. 그 재실행 여부 옵션 입니다.
+    enabled: !!isClicked,
     refetchOnWindowFocus: false,
   });
 };
