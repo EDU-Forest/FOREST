a03178dd8127f373f8831146dafe26a24b367781
choi1087 <adcdm87@gmail.com>
2023-05-01 16:31:33 +0900

    [BE-1003] feat: yml파일 수정

--- forest-back/forest-auth/src/main/java/com/ssafy/forestauth/config/SecurityConfig.java
+++ forest-back/forest-auth/src/main/java/com/ssafy/forestauth/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,6 +27,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig implements WebMvcConfigurer {
     private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
     private final CustomOAuth2UserService customOAuth2UserService;
@@ -69,7 +71,7 @@ public class SecurityConfig implements WebMvcConfigurer {
                 // JWT
                 .and()
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(jwtFilter, JwtFilter.class)
+//                .addFilterBefore(jwtFilter, JwtFilter.class)
 
                 // 예외 처리
                 .exceptionHandling()
