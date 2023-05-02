89ab525e79ebc38ffa1b5f9b24297675c21be174
ZoD <93974908+Yonghyunc@users.noreply.github.com>
2023-05-02 17:46:44 +0900

    [FE-3010] feat: 클래스(선생님) 학생 자동 검색 API call 성공
    Former-commit-id: 3613e5b0e5eef887e6fea23b5aff2bf96078b4e7

--- forest-front/src/apis/class/teacher/useClassStudentAdd.ts
+++ forest-front/src/apis/class/teacher/useClassStudentAdd.ts
@@ -2,20 +2,37 @@ import authAxios from "@/utils/authAxios";
 import { useMutation, useQueryClient } from "react-query";
 import * as queryKeys from "@/constants/queryKeys";
 
-const fetcher = (studentList: []) =>
+interface userId {
+  userId: number;
+}
+
+interface Payload {
+  classId: number;
+  studentList: userId[];
+}
+
+const fetcher = (payload: Payload) =>
   authAxios
     .post("/api/class/student", {
-      studentList,
+      classId: payload.classId,
+      studentList: payload.studentList,
     })
     .then(({ data }) => data);
 
 // 클래스에 학생 추가
-const useClassStudentAdd = (studentList: []) => {
+const useClassStudentAdd = () => {
   const queryClient = useQueryClient();
   return useMutation(fetcher, {
     onSuccess: (data) => {
       // 학생 추가 후 목록 재조회
+      console.log("하하 성공하니?", data);
       return queryClient.invalidateQueries(queryKeys.CLASS_STUDENT_LIST);
+    },
+    onSettled(data, error, variables, context) {
+      console.log("variables", variables);
+    },
+    onError: (error, variables) => {
+      console.log("하하하", variables);
     },
   });
 };
--- forest-front/src/apis/class/teacher/useSearchClassStudentQuery.ts
+++ forest-front/src/apis/class/teacher/useSearchClassStudentQuery.ts
@@ -2,12 +2,14 @@ import { useQuery } from "react-query";
 import * as queryKeys from "@/constants/queryKeys";
 import authAxios from "@/utils/authAxios";
 
-const fetcher = (userName: string) =>
-  authAxios.get("/api/user/search", { params: { userName } }).then(({ data }) => data);
+const fetcher = (userName: string, classId: number) =>
+  authAxios
+    .get("/api/user/search", { params: { classId, userName } })
+    .then(({ data }) => data.data);
 
 // 학생 자동 검색
-const useSearchClassStudentQuery = (userName: string) => {
-  return useQuery([queryKeys.SEARCH_CLASS_STUDENT, userName], () => fetcher(userName), {
+const useSearchClassStudentQuery = (userName: string, classId: number) => {
+  return useQuery([queryKeys.SEARCH_CLASS_STUDENT, userName], () => fetcher(userName, classId), {
     enabled: !!userName,
   });
 };
