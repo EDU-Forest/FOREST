f76b0cd77b38d176ea2502d276ba574fcac1a56f
HeeJeYang <ghjadmb21@gmail.com>
2023-05-03 06:58:08 +0900

    [FE-2008] fix: 전역 변수 값 변경
    Former-commit-id: 04919a4cccaa97218c4744e55a1841a454da9c64

--- forest-front/src/stores/exam/exam.ts
+++ forest-front/src/stores/exam/exam.ts
@@ -1,77 +1,120 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState: examState = {
+  // curProblemNum: 1,
+  // examTitle: "임시 시험 제목", // 임시로 넣음
+  // volume: 10,
+  // startTime: new Date("2023-01-01"),
+  // endTime: new Date(),
+  // problem: [
+  //   {
+  //     studentStudyProblemId: 1,
+  //     problemNum: 1,
+  //     type: "MULTIPLE",
+  //     title: "나는야 객관식",
+  //     text: "It is very unfortunate that I need to be drafting this letter. However, the worsening situation has forced me to submit a formal complaint. I have been renting from you for 0 years and you would agree that there is no doubt that I have paid rent every month either on time or even sometimes early. However, despite this, there has been no effort whatsoever on your part to deal with the noise disturbance of the neighboring unit I have brought to your attention multiple times. The noise which I have to deal with in the wee hours is terrible. I am afraid that I cannot stand the loud music happening at all hours in the next unit any more. Besides this, they leave a pile of garbage at my doorstep almost every other day. I hope you look into this matter at your earliest convenience.",
+  //     problemImgPath: "",
+  //     userAnswer: "",
+  //     problemAnswer: "",
+  //     item: [
+  //       {
+  //         itemId: 1,
+  //         no: 1,
+  //         content: "나는 1번 보기",
+  //         isImage: false,
+  //       },
+  //       {
+  //         itemId: 2,
+  //         no: 2,
+  //         content: "나는 2번 보기",
+  //         isImage: false,
+  //       },
+  //       {
+  //         itemId: 3,
+  //         no: 3,
+  //         content: "나는 3번 보기",
+  //         isImage: false,
+  //       },
+  //       {
+  //         itemId: 4,
+  //         no: 4,
+  //         content: "나는 4번 보기",
+  //         isImage: false,
+  //       },
+  //     ],
+  //   },
+  // ],
   curProblemNum: 1,
   examTitle: "임시 시험 제목", // 임시로 넣음
   volume: 10,
-  timeLimit: 0,
-  problems: [
+  startTime: new Date("2023-01-01"),
+  endTime: new Date(),
+  problem: [
     {
-      userAnswer: 4,
+      studentStudyProblemId: 1,
       problemNum: 1,
       type: "MULTIPLE",
-      title: "나는야 문제 제목 2",
+      title: "나는야 객관식",
       text: "It is very unfortunate that I need to be drafting this letter. However, the worsening situation has forced me to submit a formal complaint. I have been renting from you for 0 years and you would agree that there is no doubt that I have paid rent every month either on time or even sometimes early. However, despite this, there has been no effort whatsoever on your part to deal with the noise disturbance of the neighboring unit I have brought to your attention multiple times. The noise which I have to deal with in the wee hours is terrible. I am afraid that I cannot stand the loud music happening at all hours in the next unit any more. Besides this, they leave a pile of garbage at my doorstep almost every other day. I hope you look into this matter at your earliest convenience.",
-      items: [
+      problemImgPath: "",
+      userAnswer: "",
+      problemAnswer: "",
+      item: [
         {
-          itemId: 1234,
+          itemId: 1,
           no: 1,
           content: "나는 1번 보기",
           isImage: false,
         },
         {
-          itemId: 1234,
-          no: 1,
+          itemId: 2,
+          no: 2,
           content: "나는 2번 보기",
           isImage: false,
         },
         {
-          itemId: 1234,
-          no: 1,
+          itemId: 3,
+          no: 3,
           content: "나는 3번 보기",
           isImage: false,
         },
         {
-          itemId: 1234,
-          no: 1,
+          itemId: 4,
+          no: 4,
           content: "나는 4번 보기",
           isImage: false,
         },
       ],
     },
-
     {
-      userAnswer: 2,
+      studentStudyProblemId: 2,
       problemNum: 2,
-      type: "MULTIPLE",
-      title: "나는야 문제 제목",
-      text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      items: [
-        {
-          itemId: 1234,
-          no: 1,
-          content: "Lorem Ipsum",
-          isImage: false,
-        },
-        {
-          itemId: 1234,
-          no: 1,
-          content: "Lorem Ipsum",
-          isImage: false,
-        },
-        {
-          itemId: 1234,
-          no: 1,
-          content: "Lorem Ipsum",
-          isImage: false,
-        },
-        {
-          itemId: 1234,
-          no: 1,
-          content: "Lorem Ipsum",
-          isImage: false,
-        },
-      ],
+      type: "SUBJECTIVE",
+      title: "나는야 주관식",
+      text: "It is very unfortunate that I need to be drafting this letter. However, the worsening situation has forced me to submit a formal complaint. I have been renting from you for 0 years and you would agree that there is no doubt that I have paid rent every month either on time or even sometimes early. However, despite this, there has been no effort whatsoever on your part to deal with the noise disturbance of the neighboring unit I have brought to your attention multiple times. The noise which I have to deal with in the wee hours is terrible. I am afraid that I cannot stand the loud music happening at all hours in the next unit any more. Besides this, they leave a pile of garbage at my doorstep almost every other day. I hope you look into this matter at your earliest convenience.",
+      problemImgPath: "",
+      userAnswer: "",
+      problemAnswer: "",
+    },
+    {
+      studentStudyProblemId: 3,
+      problemNum: 3,
+      type: "DESCRIPT",
+      title: "나는야 서술형",
+      text: "It is very unfortunate that I need to be drafting this letter. However, the worsening situation has forced me to submit a formal complaint. I have been renting from you for 0 years and you would agree that there is no doubt that I have paid rent every month either on time or even sometimes early. However, despite this, there has been no effort whatsoever on your part to deal with the noise disturbance of the neighboring unit I have brought to your attention multiple times. The noise which I have to deal with in the wee hours is terrible. I am afraid that I cannot stand the loud music happening at all hours in the next unit any more. Besides this, they leave a pile of garbage at my doorstep almost every other day. I hope you look into this matter at your earliest convenience.",
+      problemImgPath: "",
+      userAnswer: "",
+      problemAnswer: "",
+    },
+    {
+      studentStudyProblemId: 4,
+      problemNum: 4,
+      type: "OX",
+      title: "나는야 OX",
+      text: "It is very unfortunate that I need to be drafting this letter. However, the worsening situation has forced me to submit a formal complaint. I have been renting from you for 0 years and you would agree that there is no doubt that I have paid rent every month either on time or even sometimes early. However, despite this, there has been no effort whatsoever on your part to deal with the noise disturbance of the neighboring unit I have brought to your attention multiple times. The noise which I have to deal with in the wee hours is terrible. I am afraid that I cannot stand the loud music happening at all hours in the next unit any more. Besides this, they leave a pile of garbage at my doorstep almost every other day. I hope you look into this matter at your earliest convenience.",
+      problemImgPath: "",
+      userAnswer: "",
+      problemAnswer: "",
     },
   ],
 };
@@ -80,8 +123,14 @@ const examSlice = createSlice({
   name: "exam",
   initialState,
   reducers: {
+    setStudyProblems(state, action) {
+      state = {
+        ...state,
+        ...action.payload,
+      };
+    },
     setChooseAnswer(state, action) {
-      state.problems[action.payload.problemNum - 1].userAnswer = action.payload.userAnswer;
+      state.problem[action.payload.problemNum - 1].userAnswer = action.payload.userAnswer;
     },
     setCurProblemNum(state, action) {
       state.curProblemNum = action.payload.curProblemNum;
@@ -89,6 +138,6 @@ const examSlice = createSlice({
   },
 });
 
-export const { setChooseAnswer, setCurProblemNum } = examSlice.actions;
+export const { setChooseAnswer, setCurProblemNum, setStudyProblems } = examSlice.actions;
 
 export default examSlice.reducer;
