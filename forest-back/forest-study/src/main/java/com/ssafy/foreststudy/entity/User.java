1e6ea549042109aa5f1a3e422332b9546a8d31d0
gusrnqq@naver.com <gusrnqq@naver.com>
2023-05-02 09:24:28 +0900

    [BE-3007] refactor: user pw length 수정

--- forest-back/forest-study/src/main/java/com/ssafy/foreststudy/entity/User.java
+++ forest-back/forest-study/src/main/java/com/ssafy/foreststudy/entity/User.java
@@ -32,7 +32,7 @@ public class User {
     @Column(name = "name", columnDefinition = "varchar(20)", nullable = false)
     private String name;
 
-    @Column(name = "password", columnDefinition = "varchar(20)")
+    @Column(name = "password", columnDefinition = "varchar(60)")
     private String password;
 
     @Column(name = "phone", columnDefinition = "varchar(20)")
