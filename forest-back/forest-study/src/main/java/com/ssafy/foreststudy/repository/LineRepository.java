98b6093ae57642f265d76baa8362f0bb78502df3
gusrnqq@naver.com <gusrnqq@naver.com>
2023-05-03 20:49:16 +0900

    [BE-3009] chore: library 수정

---
+++ forest-back/forest-study/src/main/java/com/ssafy/foreststudy/repository/LineRepository.java
@@ -0,0 +1,12 @@
+//package com.ssafy.foreststudy.repository;
+//
+//import com.ssafy.foreststudy.entity.canvas.Line;
+//import org.springframework.data.mongodb.repository.MongoRepository;
+//
+//import java.util.Optional;
+//
+//
+//public interface LineRepository extends MongoRepository<Line, String> {
+//
+//    //public Optional<Line> findAllById(String id);
+//}
