3c9ad35b2861b82e1fc98a30983168dd1e9b18be
choi1087 <adcdm87@gmail.com>
2023-05-03 16:21:15 +0900

    [BE-1004] refactor: test reissue accessToken

--- forest-back/forest-auth/src/main/java/com/ssafy/forestauth/auth/jwt/JwtProvider.java
+++ forest-back/forest-auth/src/main/java/com/ssafy/forestauth/auth/jwt/JwtProvider.java
@@ -25,7 +25,7 @@ public class JwtProvider {
     private String secretKey;
 
 //    public static final Long accessTokenValidateTime = 1000L * 60 * 60 * 24; // 하루
-    public static final Long accessTokenValidateTime = 1000L * 5; // 5초
+    public static final Long accessTokenValidateTime = 1000L * 10; // 10초
     public Long refreshTokenValidateTime = 1000L * 60 * 60 * 24 * 7; // 1주일
     private final String authoritiesKey = "role";
 
