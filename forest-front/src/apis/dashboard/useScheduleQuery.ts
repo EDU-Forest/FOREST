01cffeefd5c148ad48b3317101bf729dbff48735
ZoD <93974908+Yonghyunc@users.noreply.github.com>
2023-05-03 16:23:17 +0900

    [FE-3012] feat: 대시보드 스케줄 조회 API call 완료
    Former-commit-id: 21487b98b9150bd2e38685c88f802e6555c07687

--- forest-front/src/apis/dashboard/useScheduleQuery.ts
+++ forest-front/src/apis/dashboard/useScheduleQuery.ts
@@ -2,7 +2,7 @@ import { useQuery } from "react-query";
 import * as queryKeys from "@/constants/queryKeys";
 import studyAxios from "@/utils/studyAxios";
 
-const fetcher = () => studyAxios.get(`/api/study/calendar/`).then(({ data }) => data.data);
+const fetcher = () => studyAxios.get(`/api/study/calendar`).then(({ data }) => data.data);
 
 const useScheduleQuery = () => {
   return useQuery([queryKeys.GET_SCHEDULE], fetcher, {
