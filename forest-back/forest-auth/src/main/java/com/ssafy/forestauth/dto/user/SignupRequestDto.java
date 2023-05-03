39accebc1a9be251445f3c7564d5c797b78cbccb
choi1087 <adcdm87@gmail.com>
2023-05-03 10:45:56 +0900

    [BE-1004] feat: add validation in UserController

--- forest-back/forest-auth/src/main/java/com/ssafy/forestauth/dto/user/SignupRequestDto.java
+++ forest-back/forest-auth/src/main/java/com/ssafy/forestauth/dto/user/SignupRequestDto.java
@@ -5,16 +5,35 @@ import com.ssafy.forestauth.enumeration.EnumUserProviderStatus;
 import com.ssafy.forestauth.enumeration.EnumUserRoleStatus;
 import lombok.Getter;
 
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Getter
 public class SignupRequestDto {
+    @NotNull(message = "이름은 필수 값입니다!!")
+    @NotBlank(message = "이름이 빈 문자열입니다!!")
+    @Size(min = 2, max = 8, message = "이름의 길이는 2~8입니다")
     private String name; // 2~8자 길이 제한 두기
+    @NotNull(message = "이메일은 필수 값입니다.")
+    @NotBlank(message = "이메일이 빈 문자열입니다!!")
+    @Email(message = "이메일 형식이 아닙니다!!")
     private String email;
+    @NotNull(message = "전화번호는 필수 값입니다!!")
+    @NotBlank(message = "전화번호가 빈 문자열입니다!!")
     private String phone;
+    @NotNull(message = "비밀번호는 필수 값입니다!!")
+    @NotBlank(message = "비밀번호가 빈 문자열입니다!!")
     private String pw;
+    @NotNull(message = "확인 비밀번호는 필수 값입니다!!")
+    @NotBlank(message = "확인 비밀번호가 빈 문자열입니다!!")
     private String pw2;
+    @NotNull(message = "생년월일은 필수 값입니다!!")
+    @NotBlank(message = "생년월일이 빈 문자열입니다!!")
     private LocalDate birth;
+    @NotNull(message = "role은 필수 값입니다!!")
+    @NotBlank(message = "role이 빈 문자열입니다!!")
     private EnumUserRoleStatus role;
+    @NotNull(message = "provider는 필수 값입니다!!")
+    @NotBlank(message = "provider가 빈 문자열입니다!!")
     private EnumUserProviderStatus provider;
 }
--- forest-back/forest-auth/src/main/java/com/ssafy/forestauth/dto/user/SignupSocialRequestDto.java
+++ forest-back/forest-auth/src/main/java/com/ssafy/forestauth/dto/user/SignupSocialRequestDto.java
@@ -4,14 +4,28 @@ import com.ssafy.forestauth.enumeration.EnumUserProviderStatus;
 import com.ssafy.forestauth.enumeration.EnumUserRoleStatus;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
 public class SignupSocialRequestDto {
+    @NotNull(message = "이름은 필수 값입니다!!")
+    @NotBlank(message = "이름이 빈 문자열입니다!!")
+    @Size(min = 2, max = 8, message = "이름의 길이는 2~8입니다")
     private String name;
+    @NotNull(message = "전화번호는 필수 값입니다!!")
+    @NotBlank(message = "전화번호가 빈 문자열입니다!!")
     private String phone;
+    @NotNull(message = "생년월일은 필수 값입니다!!")
+    @NotBlank(message = "생년월일이 빈 문자열입니다!!")
     private LocalDate birth;
+    @NotNull(message = "role은 필수 값입니다!!")
+    @NotBlank(message = "role이 빈 문자열입니다!!")
     private EnumUserRoleStatus role;
+    @NotNull(message = "provider는 필수 값입니다!!")
+    @NotBlank(message = "provider가 빈 문자열입니다!!")
     private EnumUserProviderStatus provider;
-    private String refreshToken;
 }
