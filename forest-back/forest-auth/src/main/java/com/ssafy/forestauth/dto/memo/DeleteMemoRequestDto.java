9603bbc3487096b7890eb1f572ee121e0b7014ef
choi1087 <adcdm87@gmail.com>
2023-05-03 10:25:52 +0900

    [BE-1004] feat: add validation in MemoController

--- forest-back/forest-auth/src/main/java/com/ssafy/forestauth/dto/memo/DeleteMemoRequestDto.java
+++ forest-back/forest-auth/src/main/java/com/ssafy/forestauth/dto/memo/DeleteMemoRequestDto.java
@@ -2,8 +2,12 @@ package com.ssafy.forestauth.dto.memo;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 public class DeleteMemoRequestDto {
+    @NotNull(message = "memoId는 필수 값입니다!!")
     private Long memoId;
+    @NotNull(message = "memo 삭제 여부는 필수 값입니다!!")
     private Boolean isDeleted;
 }
--- forest-back/forest-auth/src/main/java/com/ssafy/forestauth/dto/memo/SaveMemoRequestDto.java
+++ forest-back/forest-auth/src/main/java/com/ssafy/forestauth/dto/memo/SaveMemoRequestDto.java
@@ -2,7 +2,12 @@ package com.ssafy.forestauth.dto.memo;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Getter
 public class SaveMemoRequestDto {
+    @NotNull(message = "memo content는 필수 값입니다!!")
+    @NotEmpty(message = "memo content가 빈 문자열입니다!!")
     private String content;
 }
