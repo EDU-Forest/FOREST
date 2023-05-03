1155ed22b44298696767003979232ccbbbc33124
ZoD <93974908+Yonghyunc@users.noreply.github.com>
2023-05-03 10:32:30 +0900

    [FE-3011] feat: 클래스 없는 경우 화면 추가
    Former-commit-id: 389b98a1120dcd3c934a8fb11a4824b51887b4c1

--- forest-front/src/pages/teacher/class/index.tsx
+++ forest-front/src/pages/teacher/class/index.tsx
@@ -1,16 +1,19 @@
 import TeacherNav from "@/components/Nav/TeacherNav";
 import ClassSelect from "@/features/class/ClassSelect";
 import ClassWorkbook from "@/features/class/ClassWorkbook";
+import NoClass from "@/features/class/NoClass";
 import ClassStudentList from "@/features/class/teacher/ClassStudentList";
 import ClassSummaryTeacher from "@/features/class/teacher/ClassSummaryTeacher";
 import { closeAllModal } from "@/stores/class/classModal";
+import { RootState } from "@/stores/store";
 import { Container, FullScreen } from "@/styles/container";
 import { Title } from "@/styles/text";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function TeacherClass() {
   const dispatch = useDispatch();
+  const { nowClassId } = useSelector((state: RootState) => state.class);
 
   useEffect(() => {
     dispatch(closeAllModal());
@@ -20,11 +23,17 @@ export default function TeacherClass() {
     <FullScreen>
       <TeacherNav nowLocation={"class"} />
       <Container padding={3}>
-        <ClassSelect />
-        <ClassSummaryTeacher />
-        <Title>문제집</Title>
-        <ClassWorkbook />
-        <ClassStudentList />
+        {nowClassId !== -1 ? (
+          <>
+            <ClassSelect />
+            <ClassSummaryTeacher />
+            <Title>문제집</Title>
+            <ClassWorkbook />
+            <ClassStudentList />
+          </>
+        ) : (
+          <NoClass />
+        )}
       </Container>
     </FullScreen>
   );
