fa5ebba61b55ddae8eb4a8c488ce8e8002d02eb7
ZoD <93974908+Yonghyunc@users.noreply.github.com>
2023-05-04 17:59:42 +0900

    [FE-3014] feat: 북마크 취소, 삭제 API call
    Former-commit-id: 6aedb795e2da59e922926c7d34e52180dae098e8

--- forest-front/src/apis/search/useBookmarkDelete.ts
+++ forest-front/src/apis/search/useBookmarkDelete.ts
@@ -1,13 +1,17 @@
 import workbookAxios from "@/utils/workbookAxios";
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
+import * as queryKeys from "@/constants/queryKeys";
 
 const fetcher = (workbookId: number) =>
   workbookAxios.delete(`/api/workbook/bookmark/${workbookId}`);
 
 const useBookmarkDelete = () => {
+  const queryClient = useQueryClient();
   return useMutation(fetcher, {
     onSuccess: (data) => {
       console.log("북마크 해제 ", data);
+      queryClient.invalidateQueries(queryKeys.RECENT_WORKBOOK_LIST);
+      queryClient.invalidateQueries(queryKeys.POPULAR_WORKBOOK_LIST);
     },
   });
 };
--- forest-front/src/apis/search/useBookmarkPatch.ts
+++ forest-front/src/apis/search/useBookmarkPatch.ts
@@ -1,13 +1,17 @@
 import workbookAxios from "@/utils/workbookAxios";
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
+import * as queryKeys from "@/constants/queryKeys";
 
 const fetcher = (workbookId: number) =>
   workbookAxios.patch(`/api/workbook/bookmark/${workbookId}`).then(({ data }) => data);
 
 const useBookmarkPatch = () => {
+  const queryClient = useQueryClient();
   return useMutation(fetcher, {
     onSuccess: (data) => {
       console.log("북마크 성공", data);
+      queryClient.invalidateQueries(queryKeys.RECENT_WORKBOOK_LIST);
+      queryClient.invalidateQueries(queryKeys.POPULAR_WORKBOOK_LIST);
     },
   });
 };
