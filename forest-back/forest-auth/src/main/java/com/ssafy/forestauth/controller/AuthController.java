aec9dcd267b14d298505c76d4e4ad23c016d744e
choi1087 <adcdm87@gmail.com>
2023-05-04 10:02:44 +0900

    [BE-1004] refactor: cookie refresh token log 추가

--- forest-back/forest-auth/src/main/java/com/ssafy/forestauth/controller/AuthController.java
+++ forest-back/forest-auth/src/main/java/com/ssafy/forestauth/controller/AuthController.java
@@ -32,8 +32,6 @@ public class AuthController {
         accessToken = accessToken.substring(7);
         log.info("Here is Auth Controller");
         log.info("refreshToken : {}",SecurityUtil.getCookie(request, "forest_refresh_token").toString());
-        System.out.println(SecurityUtil.getCookie(request, "forest_refresh_token"));
-        System.out.println(SecurityUtil.getCookie(request, "forest_refresh_token").get());
 
         String refreshToken = SecurityUtil.getCookie(request, "forest_refresh_token")
                 .orElseThrow(() -> new CustomException(ErrorCode.AUTH_REFRESH_NOT_VALID))
