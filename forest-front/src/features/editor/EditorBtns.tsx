8a5e4fc33e9eb7a310f57cd20cfb566353918d07
aeebbr <aeebbr@gmail.com>
2023-05-05 17:11:26 +0900

    [FE-1009] feat: 에디터 저장 api call
    Former-commit-id: d69311743398254a22c8ee9f2a57a3febead82a8

--- forest-front/src/features/editor/EditorBtns.tsx
+++ forest-front/src/features/editor/EditorBtns.tsx
@@ -1,10 +1,17 @@
 import { StyledRoundSolid600Btn } from "@/components/Button/Btn.style";
 import { EditorBtnsBox } from "./Editor.style";
+import useEditorSave from "@/hooks/editor/useEditorSave";
 
 function EditorBtns() {
+  const { editorSave } = useEditorSave();
+
+  const handleClickSave = () => {
+    editorSave();
+  };
+
   return (
     <EditorBtnsBox>
-      <StyledRoundSolid600Btn>완료</StyledRoundSolid600Btn>
+      <StyledRoundSolid600Btn onClick={handleClickSave}>완료</StyledRoundSolid600Btn>
     </EditorBtnsBox>
   );
 }
