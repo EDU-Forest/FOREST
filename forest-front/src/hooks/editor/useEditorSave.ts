29d739d8f5c1d2c84e60f0a1a7eae5ff49825576
HeeJeYang <ghjadmb21@gmail.com>
2023-05-05 21:58:57 +0900

    [FE-2011] feat: dummyData 대신 실데이터 값 교체
    Former-commit-id: 29c9e2431ffb7b024f247ce8cafbb8992e80b1ea

--- forest-front/src/hooks/editor/useEditorSave.ts
+++ forest-front/src/hooks/editor/useEditorSave.ts
@@ -7,10 +7,11 @@ function useEditorSave() {
   const { questions } = useSelector((state: RootState) => state.editQuestions);
   const { deleteAnswers } = useSelector((state: RootState) => state.editQuestions);
   const { data: res, mutate: saveApi } = useSaveEditedWorkbookPost();
+  const { curWorkbookIdx, workbooksBySelf } = useSelector(
+    (state: RootState) => state.editorWorkbook,
+  );
 
-  // dummy id
-  const id = 1;
-  const workbookId: string = id.toString();
+  const workbookId: string = workbooksBySelf[curWorkbookIdx].workbookId.toString();
   const problemList = questions.map((question: QuestionType, i) => {
     return {
       problemNo: (i + 1).toString(),
