514cdf162db545c16e8563228e749a8200ae69b0
HeeJeYang <ghjadmb21@gmail.com>
2023-05-05 21:58:10 +0900

    [FE-2011] feat: 문제풀이 페이지 UI 보완
    Former-commit-id: 8b33397863ecac0d6a47d123ee55e439823baf61

--- forest-front/src/pages/test/[studyId]/index.tsx
+++ forest-front/src/pages/test/[studyId]/index.tsx
@@ -4,15 +4,68 @@ import { StyledTestContainer } from "@/features/test/index/TextIndex.style";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import TestEndModal from "@/features/test/index/TestEndModal";
+import { dateToMinute, dateToSecond } from "@/utils";
+import { useSelector } from "react-redux";
+import { RootState } from "@/stores/store";
+import TestEnd from "@/features/test/index/TestEnd";
 
 export default function Test() {
+  const { endTime } = useSelector((state: RootState) => state.exam);
+
   const [toggleModal, setToggleModal] = useState(false);
+  const [minutes, setMinutes] = useState(dateToMinute(new Date(), endTime));
+  const [seconds, setSeconds] = useState(dateToSecond(new Date(), endTime) % 60);
+
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      }
+      if (seconds <= 0) {
+        if (minutes <= 0) {
+          clearInterval(countdown);
+        } else {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        }
+      }
+    }, 1000);
+    return () => clearInterval(countdown);
+  }, [minutes, seconds]);
 
   return (
     <StyledTestContainer>
+      {/* 테스트용 */}
       {toggleModal && <TestEndModal setToggleModal={setToggleModal} />}
-      <TestHeader page={"study"} setToggleModal={setToggleModal} />
-      <TestContent toggleModal={toggleModal} setToggleModal={setToggleModal} />
+      <TestHeader
+        page={"study"}
+        minutes={minutes}
+        seconds={seconds}
+        setToggleModal={setToggleModal}
+      />
+      {minutes > 0 && seconds > 0 ? (
+        <TestEnd />
+      ) : (
+        <TestContent
+          minutes={minutes}
+          seconds={seconds}
+          toggleModal={toggleModal}
+          setToggleModal={setToggleModal}
+        />
+      )}
+      {/* 아래가 원본 */}
+      {/* {toggleModal && <TestEndModal setToggleModal={setToggleModal} />}
+      <TestHeader
+        page={"study"}
+        minutes={minutes}
+        seconds={seconds}
+        setToggleModal={setToggleModal}
+      />
+      {minutes <= 0 && seconds <= 0 ? (
+        <TestEnd />
+      ) : (
+        <TestContent toggleModal={toggleModal} setToggleModal={setToggleModal} />
+      )} */}
     </StyledTestContainer>
   );
 }
