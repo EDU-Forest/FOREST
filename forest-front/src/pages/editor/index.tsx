15458188d9b047d6c54cd936d57f7682080aa134
aeebbr <aeebbr@gmail.com>
2023-05-05 15:52:50 +0900

    [FE-1009] feat: 에디터 문제 상세 api call
    Former-commit-id: ede30262609c85861abb36198e3d832d976c1163

--- forest-front/src/pages/editor/index.tsx
+++ forest-front/src/pages/editor/index.tsx
@@ -17,16 +17,25 @@ import { RootState } from "@/stores/store";
 import { Container, FullScreen } from "@/styles/container";
 import { useSelector } from "react-redux";
 import useGetWorkbooksBySelf from "@/apis/editor/useGetWorkbooksBySelfQuery";
+import useWorkbookDetailQuery from "@/apis/workbookDetail/useWorkbookDetailQuery";
 
 export default function Editor() {
+  const dispatch = useDispatch();
+
   const { isOpenModal } = useSelector((state: RootState) => state.editorModal);
 
   const [workbooksBySelf, setWorkbooksBySelf] = useState<IWorkbookBySelf[]>();
   const [selectQuestionType, setSelectQuestionType] = useState("");
   // const [questions, setQuestions] = useState<QuestionType[]>([]);
 
-  const dispatch = useDispatch();
   useGetWorkbooksBySelf({ setWorkbooksBySelf });
+
+  // 현재 문제집 id가 dummy. 추후 수정
+  const {} = useWorkbookDetailQuery(Number(1));
+
+  useEffect(() => {
+    dispatch(setQuestions);
+  }, []);
 
   return (
     <FullScreen>
