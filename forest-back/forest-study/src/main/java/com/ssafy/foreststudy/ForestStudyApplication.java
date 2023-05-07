c1ebc022e185647a8a81ec01b431ff1339e66323
Leeh9 <40704078+Leeh9@users.noreply.github.com>
2023-05-07 23:44:43 +0900

    [BE-3011] feat: dockerfile - timezone 추가

--- forest-back/forest-study/src/main/java/com/ssafy/foreststudy/ForestStudyApplication.java
+++ forest-back/forest-study/src/main/java/com/ssafy/foreststudy/ForestStudyApplication.java
@@ -10,10 +10,10 @@ import java.util.TimeZone;
 @SpringBootApplication
 public class ForestStudyApplication {
 
-    @PostConstruct
-    void started() {
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-    }
+//    @PostConstruct
+//    void started() {
+//        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(ForestStudyApplication.class, args);
