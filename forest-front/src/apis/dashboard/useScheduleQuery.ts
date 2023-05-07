0d7a658880fe74742eb28e31b0a95feb375f0e50
ZoD <93974908+Yonghyunc@users.noreply.github.com>
2023-05-07 23:59:01 +0900

    [FE-3013] feat: 대시보드 일정 API call response 타입 수정 및 CSS 수정
    Former-commit-id: 925a84d42b8007bb8b6f584c0dbd219a60a926e1

--- forest-front/src/apis/dashboard/useScheduleQuery.ts
+++ forest-front/src/apis/dashboard/useScheduleQuery.ts
@@ -2,7 +2,11 @@ import { useQuery } from "react-query";
 import * as queryKeys from "@/constants/queryKeys";
 import studyAxios from "@/utils/studyAxios";
 
-const fetcher = () => studyAxios.get(`/api/study/calendar`).then(({ data }) => data.data);
+const fetcher = () =>
+  studyAxios.get(`/api/study/calendar`).then(({ data }) => {
+    const studyList: ScheduleList[] = data.data.studyList;
+    return studyList;
+  });
 
 const useScheduleQuery = () => {
   return useQuery([queryKeys.GET_SCHEDULE], fetcher, {
