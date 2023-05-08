86890455c6d4fe88a3d42a648aeb55af14295996
ZoD <93974908+Yonghyunc@users.noreply.github.com>
2023-05-08 13:13:26 +0900

    [FE-3015] feat: 회원가입 폼 생일 범위 & css 수정
    Former-commit-id: ad6a3cf5c9fb0ac0a19e877e8296e1cd3a333c4f

--- forest-front/src/features/signup/Signup.style.ts
+++ forest-front/src/features/signup/Signup.style.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const SignupLayout = styled.div`
   width: 100vw;
-  padding: 10rem 0rem;
+  padding: 5rem 0rem;
   display: flex;
   justify-content: center;
 `;
