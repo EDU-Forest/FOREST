750e497d231491119f79a73461e5bc43fe009462
HeeJeYang <ghjadmb21@gmail.com>
2023-05-08 10:36:59 +0900

    [FE-2015] feat: 문제풀이 결과 분석 페이지 스크롤 수정
    Former-commit-id: 7816244c7ba7c0f7c7613d2b95ab6904a2d2a0fa

---
+++ forest-front/src/features/test/result/TestResultSection.tsx
@@ -0,0 +1,23 @@
+import { useRouter } from "next/router";
+import TestResultQuestion from "./TestResultQuestion";
+import TestResultTotal from "./TestResultTotal";
+import { TestResultOkBtn, TestResultSectionBox } from "./TextResult.style";
+import { useSelector } from "react-redux";
+import { RootState } from "@/stores/store";
+
+export default function TestResultSection() {
+  const router = useRouter();
+  const { role } = useSelector((state: RootState) => state.user);
+
+  const clickHandler = () => {
+    router.push(`/${role.toLowerCase()}/dashboard`);
+  };
+
+  return (
+    <TestResultSectionBox>
+      <TestResultTotal />
+      <TestResultQuestion />
+      <TestResultOkBtn onClick={clickHandler}>확인</TestResultOkBtn>
+    </TestResultSectionBox>
+  );
+}
