514d2bd1b14c2d5039978b8de166c6b73b848c18
HeeJeYang <ghjadmb21@gmail.com>
2023-05-08 14:48:04 +0900

    [FE-2015] feat: Nav hover 구현
    Former-commit-id: e133fa69bece9e41cbc555827f06a8e9f01bff9b

--- forest-front/src/apis/study/useGetStudyInfoQuery.ts
+++ forest-front/src/apis/study/useGetStudyInfoQuery.ts
@@ -9,15 +9,22 @@ interface Iprops {
 }
 
 const fetcher = (studyId: number) =>
-  studyAxios.get(`/api/study/info/${studyId}`).then(({ data }) => {
-    return data;
-  });
+  studyAxios
+    .get(`/api/study/info/${studyId}`)
+    .then(({ data }) => {
+      console.log("dddddddd", data);
+      return data;
+    })
+    .catch((err) => {
+      console.log("ssssssssssss", err);
+    });
 
 const useGetStudyInfo = ({ studyId }: Iprops) => {
   const dispatch = useDispatch();
   return useQuery(queryKeys.STUDY_INFO, () => fetcher(studyId), {
     refetchOnWindowFocus: false,
     onSuccess: (data) => {
+      console.log("ss", data);
       dispatch(setStudyInfo(data.data));
     },
     onError: (error) => {
--- forest-front/src/features/test/index/TestContent.tsx
+++ forest-front/src/features/test/index/TestContent.tsx
@@ -4,16 +4,29 @@ import TestProblemBox from "./TestProblemBox";
 import { StyledTestContent } from "./TextIndex.style";
 import { RootState } from "@/stores/store";
 import { useEffect, useState } from "react";
+import { CanvasPath } from "react-sketch-canvas";
+
+interface Iprops extends IStudyModal {
+  allPaths: CanvasPath[];
+  setAllPaths: (allPaths: CanvasPath[]) => void;
+}
 
 export default function TestContent({
   minutes,
   seconds,
   toggleModal,
   setToggleModal,
-}: IStudyModal) {
+  allPaths,
+  setAllPaths,
+}: Iprops) {
   return (
     <StyledTestContent>
-      <TestProblemBox minutes={minutes} seconds={seconds} />
+      <TestProblemBox
+        minutes={minutes}
+        seconds={seconds}
+        allPaths={allPaths}
+        setAllPaths={setAllPaths}
+      />
       <TestAnswerBox
         minutes={minutes}
         seconds={seconds}
