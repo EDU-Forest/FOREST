308fc6908c775431ceed912766c9b394c5c980a8
gusrnqq@naver.com <gusrnqq@naver.com>
2023-05-08 12:52:43 +0900

    [BE-3011] feat : 캘린더 API 수정

--- forest-back/forest-study/src/main/java/com/ssafy/foreststudy/service/StudyService.java
+++ forest-back/forest-study/src/main/java/com/ssafy/foreststudy/service/StudyService.java
@@ -131,8 +131,6 @@ public class StudyService {
     /* 대시보드 일정(캘린더) 목록 조회 */
     public ResponseSuccessDto<Map<String, List<GetScheduleResponseDto>>> getScheduleList(Long userId) {
 
-//        classRepository.findById(classId)
-//                .orElseThrow(() -> new CustomException(StudyErrorCode.STUDY_CLASS_NOT_FOUND));
         /* 존재하지 않는 유저 ID 체크 */
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new CustomException(StudyErrorCode.AUTH_USER_NOT_FOUND));
@@ -150,7 +148,7 @@ public class StudyService {
             }
         } else {
             List<ClassUser> classUser = classUserRepository.findAllByUser(user);
-            if(classUser.isEmpty()){
+            if (classUser.isEmpty()) {
                 throw new CustomException(StudyErrorCode.AUTH_USER_NOT_IN_CLASS);
             }
             for (ClassUser cu : classUser) {
@@ -159,26 +157,27 @@ public class StudyService {
             }
         }
 
-
-        //System.out.println("studyList = " + studyList);
-
         Map<String, List<GetScheduleResponseDto>> result = new HashMap<>();
         result.put("studyList", new ArrayList<>());
-        //List<GetScheduleResponseDto> result = new ArrayList<>();
         LocalDateTime now = LocalDateTime.now();
-        // System.out.println("now = " + now);
+
         for (Study study : studyList) {
             LocalDateTime startTime = study.getStartTime();
             LocalDateTime endTime = study.getEndTime();
+
             String schedule = null;
-            if (now.isBefore(startTime)) {
-                schedule = "BEFORE";
-            } else if (now.isAfter(endTime)) {
-                if (ChronoUnit.DAYS.between(endTime, now) > 3)
-                    continue;
-                schedule = "AFTER";
-            } else {
+            if (study.getEndTime() == null)
                 schedule = "ONGOING";
+            else {
+                if (now.isBefore(startTime))
+                    schedule = "BEFORE";
+                else if (now.isAfter(endTime)) {
+                    if (ChronoUnit.DAYS.between(endTime, now) > 3)
+                        continue;
+                    schedule = "AFTER";
+                } else
+                    schedule = "ONGOING";
+
             }
 
             GetScheduleResponseDto getScheduleResponseDto = GetScheduleResponseDto.builder()
@@ -190,7 +189,6 @@ public class StudyService {
                     .studyType(study.getType().toString())
                     .scheduleType(schedule)
                     .build();
-            //result.add(getScheduleResponseDto);
             result.get("studyList").add(getScheduleResponseDto);
         }
 
