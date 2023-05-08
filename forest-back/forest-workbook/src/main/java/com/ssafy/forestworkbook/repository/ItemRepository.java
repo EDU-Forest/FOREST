c1a2dd34699129180c615166896e38d4a9e054bb
이희진 <oliqoeiilo@naver.com>
2023-05-08 13:38:00 +0900

    [BE-2005] fix: 오타 수정

--- forest-back/forest-workbook/src/main/java/com/ssafy/forestworkbook/repository/ItemRepository.java
+++ forest-back/forest-workbook/src/main/java/com/ssafy/forestworkbook/repository/ItemRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.forestworkbook.repository;
 
 import com.ssafy.forestworkbook.entity.Item;
+import com.ssafy.forestworkbook.entity.Problem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,6 +10,6 @@ import java.util.Optional;
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
     List<Item> findAllByProblemId(Long problemId);
-    Optional<Item> findByProlbemIdAndNo(Long problemId, int no);
+    Optional<Item> findByProblemAndNo(Problem problem, int no);
 
 }
