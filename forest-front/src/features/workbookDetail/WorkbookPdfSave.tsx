b31829aa3d776055ce6d8bd210a969aa0ad3216e
임경민 <aeebbr@gmail.com>
2023-05-09 15:32:33 +0900

    [FE-1009] fix: 문제집 pdf 다운 시 이미지 포함되지 않던 문제 해결
    Former-commit-id: 190d8647d99c8ed63d59b53e04c0f94bc00e7bc8

--- forest-front/src/features/workbookDetail/WorkbookPdfSave.tsx
+++ forest-front/src/features/workbookDetail/WorkbookPdfSave.tsx
@@ -2,7 +2,7 @@ import QuestionChoiceList from "@/components/Question/QuestionChoiceList";
 import { RootState } from "@/stores/store";
 import html2canvas from "html2canvas";
 import jspdf from "jspdf";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import {
   StyledQuestionDetailNumBox,
@@ -21,86 +21,90 @@ function WorkbookPdfSave({ setIsSavePdf }: IProps) {
   const { questions } = useSelector((state: RootState) => state.editQuestions);
   const { workbook } = useSelector((state: RootState) => state.workbookDetail);
 
-  useEffect(() => {
-    const savePdf = async () => {
-      // pdf로 변환할 element의 렌더링을 캡쳐(이미지화)
-      const ele: any = document.getElementById("workbook-pdf");
-      const canvas = await html2canvas(ele);
-      const imgData = canvas.toDataURL("image/png");
+  const patchEncodedPath = (url: string) => {
+    return new Promise(async (resolve) => {
+      await fetch(url)
+        .then((response) => response.blob())
+        .then(
+          (blob) =>
+            new Promise((resolve, reject) => {
+              const reader = new FileReader();
+              reader.onloadend = () => resolve(reader.result);
+              reader.onerror = reject;
+              reader.readAsDataURL(blob);
+            }),
+        )
+        .then((response: any) => {
+          resolve(response);
+        });
+    });
+  };
 
-      console.log(canvas);
+  const convertImgSrc = () => {
+    const imgElements = document.getElementsByTagName("img");
 
-      // pdf 크기: A4 기준으로 세팅
-      const imgWidth = 210;
-      const pageHeight = imgWidth * 1.414;
-      // 이미지 크기
-      const imgHeight = (canvas.height * imgWidth) / canvas.width;
-      // 한 페이지에 삽입하고 남은 이미지 세로 영역
-      let heightLeft = imgHeight;
+    for (let i = 0; i < imgElements.length; i++) {
+      const ele = imgElements[i];
 
-      // pdf 객체
-      const doc = new jspdf("p", "mm", "a4", true);
-      //   const pageWidth = doc.internal.pageSize.getWidth();
-      //   const pageHeight = doc.internal.pageSize.getHeight();
-      let position = 0;
+      // 구글 스토리지에서 가져온 기존 이미지 경로
+      const googleStoragePath = ele.src;
+      patchEncodedPath(googleStoragePath).then((response: any) => {
+        ele.setAttribute("src", response);
+      });
+    }
+  };
 
-      // pdf에 이미지 삽입
+  const savePdf = async () => {
+    // pdf로 변환할 element의 렌더링을 캡쳐(이미지화)
+    const ele: any = document.getElementById("workbook-pdf");
+    const canvas = await html2canvas(ele);
+    const imgData = canvas.toDataURL("image/png");
+
+    // pdf 크기: A4 기준으로 세팅
+    const imgWidth = 210;
+    const pageHeight = imgWidth * 1.414;
+    // 이미지 크기
+    const imgHeight = (canvas.height * imgWidth) / canvas.width;
+    // 한 페이지에 삽입하고 남은 이미지 세로 영역
+    let heightLeft = imgHeight;
+
+    // pdf 객체
+    const doc = new jspdf("p", "mm", "a4", true);
+    //   const pageWidth = doc.internal.pageSize.getWidth();
+    //   const pageHeight = doc.internal.pageSize.getHeight();
+    let position = 0;
+
+    // pdf에 이미지 삽입
+    doc.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight, undefined, "FAST");
+
+    // 한 페이지에 삽입 후 남은 영역 계산
+    heightLeft -= pageHeight;
+
+    // 이미지를 남김 없이 삽입할 때까지 한 페이지에 삽입 반복
+    while (heightLeft >= 0) {
+      position = heightLeft - imgHeight;
+      // 새 페이지 추가하고 이미지 삽입
+      doc.addPage();
       doc.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight, undefined, "FAST");
-
       // 한 페이지에 삽입 후 남은 영역 계산
       heightLeft -= pageHeight;
+    }
 
-      // 이미지를 남김 없이 삽입할 때까지 한 페이지에 삽입 반복
-      while (heightLeft >= 0) {
-        position = heightLeft - imgHeight;
-        // 새 페이지 추가하고 이미지 삽입
-        doc.addPage();
-        doc.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight, undefined, "FAST");
-        // 한 페이지에 삽입 후 남은 영역 계산
-        heightLeft -= pageHeight;
-      }
-
-      // pdf 파일로 다운로드
-      doc.save(`${workbook.title}`);
-    };
-
-    // savePdf();
-    // pdf 저장 후 현재 컴포넌트를 렌더링 제외
-    // setIsSavePdf(false);
-  }, []);
-
-  const [img, setImg] = useState("");
-
-  function toDataURL(url: string, callback: any) {
-    var xhr = new XMLHttpRequest();
-    xhr.onload = function () {
-      var reader = new FileReader();
-      reader.onloadend = function () {
-        callback(reader.result);
-      };
-      reader.readAsDataURL(xhr.response);
-    };
-    xhr.open("GET", url);
-    xhr.responseType = "blob";
-    xhr.send();
-  }
-
-  toDataURL(
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/140px-Unofficial_JavaScript_logo_2.svg.png",
-    function (dataUrl: string) {
-      // console.log("RESULT:", dataUrl);
-    },
-  );
-
-  const test = (url: string): boolean => {
-    console.log("여기", url);
-
-    toDataURL(url, function (dataUrl: string) {
-      console.log("RESULT:", dataUrl);
-    });
-
-    return true;
+    // pdf 파일로 다운로드
+    doc.save(`${workbook.title}`);
   };
+
+  useEffect(() => {
+    // html2canvas는 외부 서버의 이미지(in 구글 스토리지 or something...)를 가져오지 못하기 때문에
+    // base64 encoding으로 변환
+    convertImgSrc();
+
+    setTimeout(() => {
+      savePdf();
+      // pdf 저장 후 현재 컴포넌트를 렌더링 제외
+      setIsSavePdf(false);
+    }, 500);
+  }, []);
 
   return (
     <WorkBookPdfBox id="workbook-pdf">
@@ -123,11 +127,7 @@ function WorkbookPdfSave({ setIsSavePdf }: IProps) {
               <StyledQuestionDetailTextBox>{question?.text}</StyledQuestionDetailTextBox>
             )}
             {/* 이미지가 있다면 이미지 렌더링 */}
-            {question?.problemImgPath && test(question?.problemImgPath) && (
-              <img src={question?.problemImgPath} alt="question" />
-            )}
-            {/* {question?.problemImgPath && <img src={question?.problemImgPath} alt="question" />} */}
-
+            {question?.problemImgPath && <img src={question?.problemImgPath} alt="question" />}
             {/* 객관식 보기 */}
             {question?.type === "MULTIPLE" && question.itemList && (
               <QuestionChoiceList items={question?.itemList} />
