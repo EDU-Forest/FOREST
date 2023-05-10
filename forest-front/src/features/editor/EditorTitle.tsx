d23a96e7d6e17f1fae1c3a4ea95a3de359e001a8
HeeJeYang <ghjadmb21@gmail.com>
2023-05-10 11:11:22 +0900

    [FE-2017] feat: 드롭다운 UI 개선
    Former-commit-id: aa83123610073db3ff8f850ae56c92a26c5d4aa6

--- forest-front/src/features/editor/EditorTitle.tsx
+++ forest-front/src/features/editor/EditorTitle.tsx
@@ -7,18 +7,27 @@ import EditorSelectDropdown from "./EditorSelectDropdown";
 import { controlEditorDropdown } from "@/stores/editor/editorModal";
 import { useEffect, useState } from "react";
 
-export default function EditorTitle() {
+interface Iprops {
+  controlDropdown: boolean;
+  setControlDropdown: (controlDropdown: boolean) => void;
+}
+
+export default function EditorTitle({ controlDropdown, setControlDropdown }: Iprops) {
   const dispatch = useDispatch();
   // const { isOpenDropdown } = useSelector((state: RootState) => state.editorModal);
-  const [controlDropdown, setControlDropdown] = useState(false);
   const { curWorkbookTitle } = useSelector((state: RootState) => state.editorWorkbook);
+
+  const controlDropdownHandler = (e: React.MouseEvent<HTMLElement | SVGElement>) => {
+    e.stopPropagation();
+    setControlDropdown(!controlDropdown);
+  };
 
   return (
     <EditorTitleBox>
       <EditorSelectedTitle>
-        {curWorkbookTitle}
+        <p onClick={controlDropdownHandler}>{curWorkbookTitle}</p>
         {/* <AiFillCaretDown onClick={() => dispatch(controlEditorDropdown())} className="icon" /> */}
-        <AiFillCaretDown onClick={() => setControlDropdown(!controlDropdown)} className="icon" />
+        <AiFillCaretDown onClick={controlDropdownHandler} className="icon" />
       </EditorSelectedTitle>
       {/* nowWorkbookId 같은 거 있어야 할 듯 */}
       {controlDropdown && <EditorSelectDropdown setControlDropdown={setControlDropdown} />}
