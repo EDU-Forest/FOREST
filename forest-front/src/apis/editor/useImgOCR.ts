f765b3aa090e99effc1bdf45c3583b3ffe0daa4d
ZoD <93974908+Yonghyunc@users.noreply.github.com>
2023-05-11 13:43:52 +0900

    [FE-3017] feat: 문제 가져오기 일부 영역 API call & 에디터에 붙여넣기
    Former-commit-id: 92e7faadb0dd755432dd240f2e8610ead54cd020

--- forest-front/src/apis/editor/useImgOCR.ts
+++ forest-front/src/apis/editor/useImgOCR.ts
@@ -1,6 +1,10 @@
 import { closePartPdfModal } from "@/stores/editor/editorModal";
+import { setQuestions } from "@/stores/editor/editorQuestions";
+import { RootState } from "@/stores/store";
+import { QuestionType } from "@/types/Workbook";
 import workbookAxios from "@/utils/customAxios/workbookAxios";
 import { useMutation } from "react-query";
+import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 
 interface IPayload {
@@ -18,10 +22,28 @@ const fetcher = (payload: IPayload) =>
     .then(({ data }) => data);
 
 const useImgOCR = () => {
+  const { questions } = useSelector((state: RootState) => state.editorQuestions);
   const dispatch = useDispatch();
   return useMutation(fetcher, {
     onSuccess: (data) => {
       console.log("이미지 OCR", data);
+      const nowIdx = questions.length;
+      const question: QuestionType = {
+        problemId: 0,
+        problemNum: nowIdx + 1,
+        type: data.data.type,
+        title: data.data.title,
+        text: data.data.text,
+        answer: "",
+        point: data.data.point,
+        problemImgPath: data.data.problemImgPath,
+        itemList: data.data.itemList,
+        imgIsEmpty: data.data.imgIsEmpty,
+        textIsEmpty: data.data.textIsEmpty,
+      };
+      const newQuestions = [...questions, question];
+      console.log(newQuestions);
+      dispatch(setQuestions(newQuestions));
       dispatch(closePartPdfModal());
     },
     onError: (err) => {
