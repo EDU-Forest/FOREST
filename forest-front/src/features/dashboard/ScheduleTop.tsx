11e2e461f5505f8edb051347a10c73ccc5f39c15
임경민 <aeebbr@gmail.com>
2023-05-11 17:34:01 +0900

    [FE-1001] fix: 대시보드 스케줄 날짜 카운트 제거
    Former-commit-id: 5e709cbd2fe59fa5ee3bbb7551c16232f3dcc801

--- forest-front/src/features/dashboard/ScheduleTop.tsx
+++ forest-front/src/features/dashboard/ScheduleTop.tsx
@@ -1,5 +1,4 @@
 import { Title } from "@/styles/text";
-import { useEffect, useState } from "react";
 import {
   StyledScheduleStatus,
   StyledScheduleStatusCircle,
@@ -8,38 +7,16 @@ import {
 } from "./Schedule.style";
 
 function ScheduleTop() {
-  const [day, setDay] = useState(1);
-
   // 오늘 날짜
   const getToday = (): string => {
     const date = new Date();
+    const day: number = new Date().getDate();
     const month: number = date.getMonth() + 1;
     const weeks: string[] = ["일", "월", "화", "수", "목", "금", "토"];
     const week: string = weeks[date.getDay()];
 
     return `${month}월 ${day}일 (${week})`;
   };
-
-  const dateCount = (): number => {
-    const day: number = new Date().getDate();
-
-    let count: number = 1;
-
-    let counting = setInterval(function () {
-      if (count == day) {
-        clearInterval(counting);
-        return false;
-      }
-      count += 1;
-      setDay(count + 1);
-    }, 40);
-
-    return count;
-  };
-
-  useEffect(() => {
-    dateCount();
-  }, []);
 
   return (
     <StyledScheduleTop>
