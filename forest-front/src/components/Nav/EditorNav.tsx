f28887658f251a27310ed487e5d2ffad53f08cd6
임경민 <aeebbr@gmail.com>
2023-05-11 16:40:37 +0900

    [FE-1009] fix: 에디터 nav에서 요소 추가 시 유효성으로 제한
    Former-commit-id: b096435e375bf2aa66f9547f047233ced5749bc1

--- forest-front/src/components/Nav/EditorNav.tsx
+++ forest-front/src/components/Nav/EditorNav.tsx
@@ -3,7 +3,9 @@ import { openPartPdfModal, openWholePdfModal } from "@/stores/editor/editorModal
 import { setCurQuestion, setQuestions } from "@/stores/editor/editorQuestions";
 import { RootState } from "@/stores/store";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import { AiFillPicture, AiOutlinePicLeft } from "react-icons/ai";
+import { IoIosWarning } from "react-icons/io";
 import {
   MdOutlineFormatListNumbered,
   MdOutlineLineStyle,
@@ -11,6 +13,7 @@ import {
 } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
 import ArrowLeft from "../Arrow/ArrowLeft";
+import Toast from "../Toast/Toast";
 import {
   ArrowDiv,
   EditorNavDiv,
@@ -30,6 +33,10 @@ export default function EditorNav({ setSelectQuestionType }: IProps) {
   const { questions } = useSelector((state: RootState) => state.editorQuestions);
   const { curQuestion } = useSelector((state: RootState) => state.editorQuestions);
   const { toChangeQuestions } = useEditor();
+  const { isPointValidConfirm } = useSelector((state: RootState) => state.editorQuestions);
+  const { isTitleValidConfirm } = useSelector((state: RootState) => state.editorQuestions);
+  const { isAnswerValidConfirm } = useSelector((state: RootState) => state.editorQuestions);
+  const [isAddFail, setIsAddFail] = useState(false);
 
   const goToDashboard = () => {
     router.push("/teacher/dashboard");
@@ -90,66 +97,106 @@ export default function EditorNav({ setSelectQuestionType }: IProps) {
   };
 
   const handleClickQuestionType = (type: string) => {
-    type === "MULTIPLE"
-      ? dispatch(setQuestions([...questions, { ...questionInit.withItems }]))
-      : dispatch(setQuestions([...questions, { ...questionInit.withoutItems, type: type }]));
-    dispatch(setCurQuestion(questions.length + 1));
+    setIsAddFail(!isPointValidConfirm || !isTitleValidConfirm || !isAnswerValidConfirm);
+
+    if (
+      questions.length === 0 ||
+      (isPointValidConfirm && isTitleValidConfirm && isAnswerValidConfirm)
+    ) {
+      type === "MULTIPLE"
+        ? dispatch(setQuestions([...questions, { ...questionInit.withItems }]))
+        : dispatch(setQuestions([...questions, { ...questionInit.withoutItems, type: type }]));
+      dispatch(setCurQuestion(questions.length + 1));
+    }
   };
 
   const handleClickObjectType = (type: string) => {
-    if (type === "text") {
-      const copyArr = [...questions];
-      copyArr.splice(curQuestion - 1, 1, { ...questions[curQuestion - 1], textIsEmpty: false });
-      dispatch(setQuestions([...copyArr]));
-    } else if (type === "image") {
-      toChangeQuestions("imgIsEmpty", false);
+    setIsAddFail(!isPointValidConfirm || !isTitleValidConfirm || !isAnswerValidConfirm);
+
+    if (
+      questions.length === 0 ||
+      (isPointValidConfirm && isTitleValidConfirm && isAnswerValidConfirm)
+    ) {
+      if (type === "text") {
+        const copyArr = [...questions];
+        copyArr.splice(curQuestion - 1, 1, { ...questions[curQuestion - 1], textIsEmpty: false });
+        dispatch(setQuestions([...copyArr]));
+      } else if (type === "image") {
+        toChangeQuestions("imgIsEmpty", false);
+      }
+    }
+  };
+
+  const handleClickImport = (type: string) => {
+    setIsAddFail(!isPointValidConfirm || !isTitleValidConfirm || !isAnswerValidConfirm);
+
+    if (
+      questions.length === 0 ||
+      (isPointValidConfirm && isTitleValidConfirm && isAnswerValidConfirm)
+    ) {
+      if (type === "whole") {
+        dispatch(openWholePdfModal());
+      } else if (type === "part") {
+        dispatch(openPartPdfModal());
+      }
     }
   };
 
   // MULTIPLE, SUBJECTIVE(주관식, 단답식), DESCRIPT(서술형), OX
   return (
-    <StyledEditorNav>
-      <ArrowDiv>
-        <ArrowLeft onClick={goToDashboard} />
-      </ArrowDiv>
-      <EditorNavDivTitle isObject={false}>새로 만들기</EditorNavDivTitle>
-      <EditorNavDiv>
-        <EditorNavDivInner onClick={() => handleClickQuestionType("MULTIPLE")}>
-          <MdOutlineFormatListNumbered className="icon" />
-          <span>객관식</span>
-        </EditorNavDivInner>
-        <EditorNavDivInner onClick={() => handleClickQuestionType("OX")}>
-          <div className="ox-div">
-            <span>O</span>
-            <span>X</span>
-          </div>
-          OX 선택
-        </EditorNavDivInner>
-        <EditorNavDivInner onClick={() => handleClickQuestionType("SUBJECTIVE")}>
-          <MdOutlinePowerInput className="icon" />
-          단답식
-        </EditorNavDivInner>
-        <EditorNavDivInner onClick={() => handleClickQuestionType("DESCRIPT")}>
-          <MdOutlineLineStyle className="icon" />
-          서술형
-        </EditorNavDivInner>
-      </EditorNavDiv>
-      <EditorNavDivTitle isObject={false}>문제 가져오기</EditorNavDivTitle>
-      <EditorNavDiv className="upload-problem">
-        <p onClick={() => dispatch(openWholePdfModal())}>전체 영역</p>
-        <p onClick={() => dispatch(openPartPdfModal())}>일부 영역</p>
-      </EditorNavDiv>
-      <EditorNavDivTitle isObject>오브젝트</EditorNavDivTitle>
-      <EditorNavDiv>
-        <EditorNavDivInner onClick={() => handleClickObjectType("image")}>
-          <AiFillPicture className="object" />
-          이미지
-        </EditorNavDivInner>
-        <EditorNavDivInner onClick={() => handleClickObjectType("text")}>
-          <AiOutlinePicLeft className="object" />
-          지문
-        </EditorNavDivInner>
-      </EditorNavDiv>
-    </StyledEditorNav>
+    <>
+      <StyledEditorNav>
+        <ArrowDiv>
+          <ArrowLeft onClick={goToDashboard} />
+        </ArrowDiv>
+        <EditorNavDivTitle isObject={false}>새로 만들기</EditorNavDivTitle>
+        <EditorNavDiv>
+          <EditorNavDivInner onClick={() => handleClickQuestionType("MULTIPLE")}>
+            <MdOutlineFormatListNumbered className="icon" />
+            <span>객관식</span>
+          </EditorNavDivInner>
+          <EditorNavDivInner onClick={() => handleClickQuestionType("OX")}>
+            <div className="ox-div">
+              <span>O</span>
+              <span>X</span>
+            </div>
+            OX 선택
+          </EditorNavDivInner>
+          <EditorNavDivInner onClick={() => handleClickQuestionType("SUBJECTIVE")}>
+            <MdOutlinePowerInput className="icon" />
+            단답식
+          </EditorNavDivInner>
+          <EditorNavDivInner onClick={() => handleClickQuestionType("DESCRIPT")}>
+            <MdOutlineLineStyle className="icon" />
+            서술형
+          </EditorNavDivInner>
+        </EditorNavDiv>
+        <EditorNavDivTitle isObject={false}>문제 가져오기</EditorNavDivTitle>
+        <EditorNavDiv className="upload-problem">
+          <p onClick={() => handleClickImport("whole")}>전체 영역</p>
+          <p onClick={() => handleClickImport("part")}>일부 영역</p>
+        </EditorNavDiv>
+        <EditorNavDivTitle isObject>오브젝트</EditorNavDivTitle>
+        <EditorNavDiv>
+          <EditorNavDivInner onClick={() => handleClickObjectType("image")}>
+            <AiFillPicture className="object" />
+            이미지
+          </EditorNavDivInner>
+          <EditorNavDivInner onClick={() => handleClickObjectType("text")}>
+            <AiOutlinePicLeft className="object" />
+            지문
+          </EditorNavDivInner>
+        </EditorNavDiv>
+      </StyledEditorNav>
+      {isAddFail && (
+        <Toast
+          icon={<IoIosWarning />}
+          subtitle="추가 불가"
+          title="문항 수정을 완료해주세요"
+          isOpen={isAddFail}
+          setIsOpen={setIsAddFail}
+        />
+      )}
+    </>
   );
 }
