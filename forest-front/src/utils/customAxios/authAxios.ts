ce3618fe95b573cad36c4b3543119634e9734742
HeeJeYang <ghjadmb21@gmail.com>
2023-05-14 20:50:41 +0900

    [FE-2020] feat: failed refresh token expired test
    Former-commit-id: 3bb89cea8b3b4dd143c0f6aae6392fc2fbef00c8

--- forest-front/src/utils/customAxios/authAxios.ts
+++ forest-front/src/utils/customAxios/authAxios.ts
@@ -1,17 +1,7 @@
 import axios, { AxiosRequestConfig } from "axios";
 import { getLocalStorage, setLocalStorage } from "../localStorage";
-import { useRouter } from "next/router";
-import { useDispatch } from "react-redux";
-import { setLogout } from "@/stores/user/user";
 
 const { NEXT_PUBLIC_SERVER_URL } = process.env;
-
-function failedAuth() {
-  const router = useRouter();
-  const dispatch = useDispatch();
-  dispatch(setLogout());
-  router.push("/");
-}
 
 const AxiosConFigure: AxiosRequestConfig = {
   baseURL: `${NEXT_PUBLIC_SERVER_URL}:9010`,
@@ -46,14 +36,9 @@ authAxios.interceptors.response.use(
       const newAccessToken = async () => {
         const response = await authAxios.get("/api/auth/reissue");
         const { token } = response.data.data;
-
         return token;
       };
       const accessToken = await newAccessToken();
-      if (!accessToken) {
-        failedAuth();
-        return;
-      }
       setLocalStorage("forest_access_token", accessToken);
       prevRequest.headers.Authorization = `Bearer ${accessToken}`;
       return authAxios(prevRequest);
