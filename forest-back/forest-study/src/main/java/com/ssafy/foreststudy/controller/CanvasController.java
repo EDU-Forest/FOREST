e9dfc5ab0f4f4c68bedea2189ac3d98ebdd0e391
Leeh9 <40704078+Leeh9@users.noreply.github.com>
2023-05-14 21:23:33 +0900

    [BE-3014] feat: library update

--- forest-back/forest-study/src/main/java/com/ssafy/foreststudy/controller/CanvasController.java
+++ forest-back/forest-study/src/main/java/com/ssafy/foreststudy/controller/CanvasController.java
@@ -4,7 +4,6 @@ import com.ssafy.forest.jwt.JwtDecoder;
 import com.ssafy.foreststudy.dto.canvas.GetCanvasResponseDto;
 import com.ssafy.foreststudy.dto.common.response.ResponseSuccessDto;
 import com.ssafy.foreststudy.dto.study.PostResponseDto;
-import com.ssafy.foreststudy.entity.canvas.Canvas;
 import com.ssafy.foreststudy.service.CanvasService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -26,11 +25,11 @@ import java.io.UnsupportedEncodingException;
 @RequestMapping("/canvas")
 public class CanvasController {
     private final CanvasService canvasService;
+    private final JwtDecoder jwtDecoder;
 
     @ApiOperation(value = "캔버스 풀이 저장", notes = "캔버스 풀이를 저장합니다.")
     @PostMapping()
     public ResponseEntity<ResponseSuccessDto<PostResponseDto>> postCanvas(HttpServletRequest request, @RequestBody @Valid GetCanvasResponseDto getCanvasResponseDto) throws UnsupportedEncodingException {
-        JwtDecoder jwtDecoder = new JwtDecoder();
         Long userId = jwtDecoder.verifyJWT(request);
         return ResponseEntity.ok(canvasService.postCanvas(getCanvasResponseDto,userId));
     }
@@ -38,7 +37,6 @@ public class CanvasController {
     @ApiOperation(value = "캔버스 풀이 조회", notes = "캔버스 풀이를 조회합니다.")
     @GetMapping("/{studentStudyProblemId}")
     public ResponseEntity<ResponseSuccessDto<GetCanvasResponseDto>> getCanvas(HttpServletRequest request, @PathVariable("studentStudyProblemId") Long studentStudyProblemId) throws UnsupportedEncodingException {
-        JwtDecoder jwtDecoder = new JwtDecoder();
         Long userId = jwtDecoder.verifyJWT(request);
         return ResponseEntity.ok(canvasService.getCanvas(studentStudyProblemId,userId));
     }
