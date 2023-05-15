479e282387d2fea5f80d75784818fa2a73053860
HeeJeYang <ghjadmb21@gmail.com>
2023-05-15 13:42:46 +0900

    [FE-2020] feat: react query 테스트
    Former-commit-id: 7dfd17953fdbd14fc3b76daacece7b4e596947b2

--- forest-front/src/pages/signup/index.tsx
+++ forest-front/src/pages/signup/index.tsx
@@ -1,6 +1,7 @@
 import UserForm from "@/features/home/UserForm";
 import { SignupContainer, SignupLayout } from "@/features/signup/Signup.style";
 import avoidDuplicateLoginAuth from "@/utils/auth/AvoidDuplicateLoginAuth";
+import { useEffect } from "react";
 
 interface Iprops {
   email: string;
@@ -14,6 +15,9 @@ interface IServerSideProps {
 }
 
 function Signup({ email, onClose }: Iprops) {
+  useEffect(() => {
+    console.log(email);
+  }, []);
   return (
     <SignupContainer>
       <UserForm type={"signup"} onClose={onClose} email={email} />
