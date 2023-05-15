f738bdf70fe39dca91f5023242374f1c5f23bc46
ZoD <93974908+Yonghyunc@users.noreply.github.com>
2023-05-15 15:01:03 +0900

    [FE-3019] feat: 사용가이드
    Former-commit-id: 7320ba8c66a2c31f0db8b0fa954194247ad983e7

---
+++ forest-front/src/features/guide/Guide.style.ts
@@ -0,0 +1,9 @@
+import { Container } from "@/styles/container";
+import styled from "styled-components";
+
+const GuideWrapper = styled(Container)`
+  width: 100%;
+  height: 100%;
+`;
+
+export { GuideWrapper };
---
+++ forest-front/src/pages/guide/index.tsx
@@ -0,0 +1,5 @@
+import { GuideWrapper } from "../../features/guide/Guide.style";
+
+export default function UserGuide() {
+  return <GuideWrapper padding={3}></GuideWrapper>;
+}
