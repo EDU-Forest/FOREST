3755a5a8e01152dd783805ce75dd619fb1fa3c99
ZoD <93974908+Yonghyunc@users.noreply.github.com>
2023-05-15 16:39:07 +0900

    [FE-3019] feat: 사용가이드 네비바 작성
    Former-commit-id: 57407ea9abb8a8410db8d3b47cba8e9673282e20

--- forest-front/src/pages/guide/index.tsx
+++ forest-front/src/pages/guide/index.tsx
@@ -1,11 +1,12 @@
 import { Container, FullScreen } from "@/styles/container";
-import { GuideWrapper } from "../../features/guide/Guide.style";
 import GuideNav from "@/features/guide/GuideNav";
+import { useState } from "react";
 
 export default function UserGuide() {
+  const [guideTab, setGuideTab] = useState<string>("intro");
   return (
     <FullScreen>
-      <GuideNav />
+      <GuideNav guideTab={guideTab} setGuideTab={setGuideTab} />
       <Container padding={3}></Container>
     </FullScreen>
   );
