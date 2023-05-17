8cca925e95b0274ad3a1322c1c56bffff2d3ee6f
이희진 <oliqoeiilo@naver.com>
2023-05-17 10:17:09 +0900

    [BE-2005] chore: config 수정
    Former-commit-id: d8d02653ed806860f9822e055f605b176bf396a4

--- forest-back/forest-workbook/src/main/java/com/ssafy/forestworkbook/config/WorkbookConfig.java
+++ forest-back/forest-workbook/src/main/java/com/ssafy/forestworkbook/config/WorkbookConfig.java
@@ -1,28 +1,14 @@
 package com.ssafy.forestworkbook.config;
 
-
-import org.apache.coyote.http2.Http2Protocol;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.boot.autoconfigure.web.embedded.NettyWebServerFactoryCustomizer;
-import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-
-import java.util.Arrays;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WorkbookConfig {
+public class WorkbookConfig implements WebMvcConfigurer {
 
-    @Bean
-    public ConfigurableServletWebServerFactory tomcatCustomizer() {
-        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-
-        Http2Protocol protocol = new Http2Protocol();
-        protocol.setOverheadDataThreshold(0);
-        factory.addConnectorCustomizers(connector -> connector.addUpgradeProtocol(protocol));
-        return factory;
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/workbook/", "/workbook");
     }
 }
