bdedc9e0a1913891b0e027360ca12813930889a9
이희진 <oliqoeiilo@naver.com>
2023-05-17 10:44:21 +0900

    [BE-2005] chore: config 수정
    Former-commit-id: 258e1954c8c52e88113a4bdf7c199adbc0f30677

--- forest-back/forest-workbook/src/main/java/com/ssafy/forestworkbook/config/WorkbookConfig.java
+++ forest-back/forest-workbook/src/main/java/com/ssafy/forestworkbook/config/WorkbookConfig.java
@@ -1,6 +1,7 @@
 package com.ssafy.forestworkbook.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,10 @@ public class WorkbookConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("/workbook/", "/workbook");
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseTrailingSlashMatch(false);
     }
 }
